4dc027f06334c99f98cc16dc3259b56b73b7077e
piry <sylvain.piry@inra.fr>
2019-03-05 13:28:55 +0100

    Transfer now applies on stable version

--- transfer2peercom.sh
+++ transfer2peercom.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-src_name="/home/piry/W/web2py_2.17.2/applications/pcidev/"
+src_name="/home/piry/W/web2py_2.17.2/applications/pci_stable/"
 dir_name="/var/www/peercommunityin/web2py217/applications/PCIEvolBiol"
 db_name="pci_evolbiol"
 db_test="pci_evolbiol_test"
@@ -19,7 +19,11 @@ echo "https://evolbiol.peercommunityin.org/public/rss" | dmtxwrite --encoding=b 
 # echo "UPDATE t_reviews SET review_state='Completed' WHERE review_state LIKE 'Terminated';" | ssh peercom@peercom-front1 "psql -h mydb1 -p 5432 -U peercom -d $db_name"
 
 # SYNC SOURCE CODE FROM MBIPI TO WEBSITE
-rsync $rsopts --stats --recursive --perms --links --update --delete --delete-before --exclude='*~' --exclude='*.pyc' --exclude='*.bak' --exclude '.git' --exclude 'sessions' --exclude 'cache' --exclude 'errors' --exclude '*.ini' --exclude '*.old' --exclude '*background.png' --exclude '*workflow*.png' $src_name peercom@peercom-front1:$dir_name
+rsync $rsopts --stats --recursive --perms --links --update --delete --delete-before --exclude='*~' \
+	--exclude='*.pyc' --exclude='*.bak' --exclude '.git' --exclude 'sessions' --exclude 'cache' \
+	--exclude 'errors' --exclude '*.ini' --exclude '*.old' --exclude '*background.png' --exclude '*workflow*.png' \
+	$src_name \
+	peercom@peercom-front1:$dir_name
 
 # cat /home/piry/W/Labo/PCiEvolBiol/2017-09-18_modifs_fonctions.sql                                  | ssh peercom@peercom-front1 "psql -h mydb1 -p 5432 -U peercom -d $db_name"
 # cat /home/piry/W/Labo/PCiEvolBiol/2017-11-13_auto_last_change_recommendation_trigger_function.sql  | ssh peercom@peercom-front1 "psql -h mydb1 -p 5432 -U peercom -d $db_name"
@@ -40,6 +44,8 @@ rsync $rsopts --stats --recursive --perms --links --update --delete --delete-bef
 # echo "ALTER TABLE public.t_recommendations ADD COLUMN recommender_file character varying(512); ALTER TABLE public.t_recommendations ADD COLUMN recommender_file_data bytea;" | ssh peercom@peercom-front1 "psql -h mydb1 -p 5432 -U peercom -d $db_name"
 # echo "ALTER TABLE public.t_suggested_recommenders ADD COLUMN emailing text;" | ssh peercom@peercom-front1 "psql -h mydb1 -p 5432 -U peercom -d $db_name"
 # echo "UPDATE help_texts SET contents='Details about the process of evaluation & recommendation can be found  [here](../about/help_generic).' WHERE hashtag LIKE '#AcceptPreprintInfoText';" | ssh peercom@peercom-front1 "psql -h mydb1 -p 5432 -U peercom -d $db_name"
+# echo "ALTER TABLE t_articles ADD COLUMN parallel_submission boolean DEFAULT false;"| ssh peercom@peercom-front1 "psql -h mydb1 -p 5432 -U peercom -d $db_name"
+# cat /home/piry/W/Labo/PCiEvolBiol/2019-02-25_SearchArticles.sql | ssh peercom@peercom-front1 "psql -h mydb1 -p 5432 -U peercom -d $db_name"
 
 #WARNING: check group!!
 # rsync $rsopt /home/piry/W/web2py/applications/pcidev/private/peercom_appconfig.ini            peercom@peercom-front1:$dir_name/private/appconfig.ini
