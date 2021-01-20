a5780296f5df1ca1591b41831941d59d7d4af884
piry <sylvain.piry@inrae.fr>
2021-01-20 11:14:17 +0100

    IMPORTANT! Change in sql script for review_state update: disable timestamp trigger before update and re-enable trigger after update

--- sql_dumps/update_t_reviews_states.sql
+++ sql_dumps/update_t_reviews_states.sql
@@ -1,4 +1,6 @@
+ALTER TABLE t_reviews DISABLE TRIGGER auto_last_change_trigger;
 UPDATE t_reviews SET review_state = 'Review completed' WHERE review_state = 'Completed';
 UPDATE t_reviews SET review_state = 'Willing to review' WHERE review_state = 'Ask to review';
 UPDATE t_reviews SET review_state = 'Awaiting review' WHERE review_state = 'Under consideration';
 UPDATE t_reviews SET review_state = 'Awaiting response' WHERE review_state = 'Pending';
+ALTER TABLE t_reviews ENABLE TRIGGER auto_last_change_trigger;
