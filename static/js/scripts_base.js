a599a6de05d34077e4de33193c9b43e82ea307f3
Timo Lubitz <timo.lubitz@gmail.com>
2021-06-28 10:50:17 +0200

    Layout/Functionality changes; response to D&T

--- static/js/scripts_base.js
+++ static/js/scripts_base.js
@@ -13,47 +13,50 @@ function lock_body() {
     /* secondly, we adapt navbar visibility on tablet in order to have 
         a different behaviour than bootstrap intends; */
     /* last, we also change the burger icon from active/inactive */
-    var body = document.querySelector('body');
+    //var body = document.querySelector('body');
     var mynavbar = document.querySelector('#myNavbar');
     var burger = document.querySelector('#pci-burger');
     if (scroll_lock == false) {
-        body.style.overflow = 'hidden';
+        //body.style.overflow = 'hidden';
         scroll_lock = true;
         if (window.innerWidth > 767) {
             mynavbar.style.display = 'block';
         }
-        burger.src = '/pci/static/images/pci-burger-active-03.svg';
+        burger.classList.add('burger-active');
+        //burger.src = '/pci/static/images/pci-burger-active-03.svg';
     }
     else {
-        body.style.overflow = 'scroll';
+        //body.style.overflow = 'scroll';
         scroll_lock = false;
         if (window.innerWidth > 767) {
             mynavbar.style.display = 'none';
         }
-        burger.src = '/pci/static/images/pci-burger-default.svg';
+        burger.classList.remove('burger-active');
+        //burger.src = '/pci/static/images/pci-burger-default.svg';
     }
 }
 
 
-function subnavs(subnav) {
+function subnavs(subnav_li) {
     // only one of the sub navigations must be open at one time
     // so that they do not overlap;
     // this function closes all sub navigations except for the one clicked
+    var subnav = subnav_li.querySelector('.subnav');
     var sub_navs = document.querySelectorAll('.subnav');
     for (var i = 0; i<sub_navs.length; i++) {
         if (sub_navs[i] != subnav) {
             sub_navs[i].classList.remove('in');
         }
-    }   
+    }
 }
 
 
 // these 2 functions serve as hover effects of the navigation burger on mobile
 function change_img() {
     var nav_burger = document.querySelector('#pci-burger');
-    nav_burger.setAttribute('src', '/pci/static/images/pci-burger-active-03.svg');
+    //nav_burger.setAttribute('src', '/pci/static/images/pci-burger-active-03.svg');
 }
 function change_back() {
     var nav_burger = document.querySelector('#pci-burger');
-    nav_burger.setAttribute('src', '/pci/static/images/pci-burger-default.svg');
+    //nav_burger.setAttribute('src', '/pci/static/images/pci-burger-default.svg');
 }
