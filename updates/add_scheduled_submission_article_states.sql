fea525cbeaec83dabac90108e2a316d701971f7a
Gabriel Franck <gabfranck.pro@gmail.com>
2021-08-27 10:45:54 +0200

    add new article status and validation process for scheduled submission

---
+++ updates/add_scheduled_submission_article_states.sql
@@ -0,0 +1,4 @@
+INSERT INTO t_status_article("status","color_class","explaination","priority_level")
+VALUES
+(E'Scheduled submission pending',E'danger',E'Scheduled submission waiting for Recommender approval',E'A'),
+(E'Scheduled submission under consideration',E'info',E'Scheduled submission accepted by Recommender',E'A');
