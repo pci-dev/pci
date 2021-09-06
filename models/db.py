3f46289fb00918ce4ded1463db924a298f106f90
ofa <fambon@github.users>
2021-09-06 09:40:30 +0200

    allow uploads of review filetypes pdf, docx, odt (pci RR only)

--- models/db.py
+++ models/db.py
@@ -46,6 +46,9 @@ port = myconf.take("alerts.port", cast=lambda v: common_tools.takePort(v))
 pdf_max_size = int(myconf.take("config.pdf_max_size") or 5)
 
 scheduledSubmissionActivated = myconf.get("config.scheduled_submissions", default=False)
+pciRRactivated = myconf.get("config.registered_reports", default=False)
+
+allowed_upload_filetypes = [ "pdf" ] if not pciRRactivated else [ "pdf", "docx", "odt" ]
 
 if not request.env.web2py_runtime_gae:
     # ---------------------------------------------------------------------
@@ -836,7 +839,8 @@ db.define_table(
         type="upload",
         uploadfield="review_pdf_data",
         label=T("Review as PDF"),
-        requires=[IS_LENGTH(pdf_max_size * 1048576, error_message="The file size is over " + str(pdf_max_size) + "MB."), IS_EMPTY_OR(IS_UPLOAD_FILENAME(extension="pdf"))],
+        requires=[IS_LENGTH(pdf_max_size * 1048576, error_message="The file size is over " + str(pdf_max_size) + "MB."),
+            IS_EMPTY_OR(IS_FILE(extension=allowed_upload_filetypes))],
     ),
     Field("review_pdf_data", type="blob", readable=False),
     Field("acceptation_timestamp", type="datetime", label=T("Acceptation timestamp"), writable=False),
