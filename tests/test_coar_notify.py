d1fd4e87eaeb3b0af33baa49afde6fc5e3427f64
ofa <fambon@github.users>
2021-11-07 20:59:53 +0100

    add basic pytest for pci notifs

---
+++ tests/test_coar_notify.py
@@ -0,0 +1,48 @@
+import io
+import rdflib
+
+pci_notif = b"""
+{
+  "@context": [
+    "https://www.w3.org/ns/activitystreams",
+    "http://purl.org/coar/notify"
+  ],
+  "id": "urn: uuid:ab19e043-81b1-4ba8-9f63-607c56c3407c",
+  "origin": {
+    "id": "http://localhost:8000/pci/coar_notify/",
+    "inbox": "http://localhost:8000/pci/coar_notify/inbox/",
+    "type": [ "Service" ]
+  },
+  "target": {
+    "inbox": "http://localhost:8000/pci/coar_notify/inbox",
+    "type": [ "Service" ]
+  },
+  "type": [
+    "Announce",
+    "coar-notify:ReviewAction"
+  ],
+  "context": {
+    "id": "http://localhost:8000/pci/articles/rec?articleId=1#article-1",
+    "ietf:cite-as": "10.1038/nature.2014.14583",
+    "type": "sorg:AboutPage"
+  },
+  "object": {
+    "id": "http://localhost:8000/pci/articles/rec?articleId=1#review-1",
+    "type": [ "Document", "sorg:Review" ]
+  },
+  "actor": {
+    "id": "http://localhost:8000/pci/public/user_public_page?userId=6",
+    "type": [ "Person" ],
+    "name": "reviewer dude"
+  }
+}
+"""
+
+def test_pci_notif_parse():
+
+    graph = rdflib.Graph()
+    graph.parse(
+            io.BytesIO(pci_notif),
+            format="json-ld",
+            base="http://127.0.0.1:8000/pci/coar_notify/inbox"
+    )
