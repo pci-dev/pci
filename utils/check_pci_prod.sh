5f73f432f6183be007e95c4a90964ae632af936e
ofa <fambon@github.users>
2021-11-17 15:09:58 +0100

    add utils/check_pci_prod.sh

---
+++ utils/check_pci_prod.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+sites=(
+evolbiol
+ecology
+paleo
+animsci
+zool
+neuro
+genomics
+mcb
+forestwoodsci
+archaeo
+networksci
+rr
+ecotoxenvchem
+infections
+compstat
+)
+
+get_site_version() {
+	site=$1
+
+	version=$(curl -s https://${site}.peercommunityin.org/about/version \
+		| grep HEAD \
+		| sed 's:).*:):' \
+		| sed 's:&gt;:>:'
+	)
+	printf "%-20s: %s\n" "$site" "$version"
+}
+
+main() {
+	tmp=$(mktemp)
+
+	rm -f $tmp
+
+	for site in ${sites[*]}; do
+		get_site_version $site >> $tmp &
+	done
+	wait
+	sort $tmp
+	rm $tmp
+}
+
+main
