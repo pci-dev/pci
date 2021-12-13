253432b27ab4513eb8ce9ebb5383ed99cb6b7b87
ofa <fambon@github.users>
2021-12-13 17:19:20 +0100

    add scheduled cleanup for help_text_old in updates/

--- updates/fix_help_texts_3.sql
+++ updates/fix_help_texts_3.sql
@@ -1,5 +1,1 @@
-alter table help_texts rename to help_text_old;
-alter table help_texts_3 rename to help_texts;
-alter sequence help_texts_id_seq owned by help_texts.id;
-
--- drop table help_text_old; -- => do that when deploying /next/ release
+drop table help_text_old; -- => do that when deploying /next/ release
