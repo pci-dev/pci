d1a57e5252cc1556cfe2f2f5d520604f18f3eda7
Michael Ajanaku <remiljw@gmail.com>
2022-01-14 09:25:06 +0100

    add sql update script

---
+++ updates/add_new_custom_text.sql
@@ -0,0 +1,7 @@
+INSERT INTO "public"."help_texts"("hashtag","lang","contents")
+VALUES
+(
+E'#RecommenderReinviteReviewersText',
+E'default',
+E'<p>For this new round of review, you can choose reviewers that reviewed the previous version AND/OR you can invite new reviewers either from the PCI Evol Biol database (green button) or outside the PCI Evol Biol database (black button).</p>\n<p>When inviting referees, keep in mind that a large gender, career stage and geographic diversity is desirable.&nbsp;&nbsp;</p>\n<p>In case of any problems or difficulties, please contact us at <a href="mailto:{{contact}}">{{contact}}</a></p>'
+);
