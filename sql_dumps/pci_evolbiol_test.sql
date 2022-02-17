3e5e8bfc0362caf07ef7da5b778054e3537dd29d
Michael Ajanaku <remiljw@gmail.com>
2022-02-17 18:16:14 +0100

    add db structure updates sql script

--- sql_dumps/pci_evolbiol_test.sql
+++ sql_dumps/pci_evolbiol_test.sql
@@ -2897,3 +2897,17 @@ ALTER TABLE ONLY public.t_user_words
 -- PostgreSQL database dump complete
 --
 
+SET search_path to public;
+
+-- 2022-02-21 updates/alter_new_submission_fields.sql
+
+ALTER TABLE "t_articles" 
+DROP COLUMN IF EXISTS no_results_based_on_data,
+DROP COLUMN IF EXISTS no_codes_used_in_study,
+DROP COLUMN IF EXISTS no_scripts_used_for_result,
+ALTER COLUMN  results_based_on_data TYPE character varying(512),
+ALTER COLUMN  scripts_used_for_result TYPE character varying(512),
+ALTER COLUMN  codes_used_in_study TYPE character varying(512),
+ALTER COLUMN  results_based_on_data DROP DEFAULT,
+ALTER COLUMN  scripts_used_for_result DROP DEFAULT,
+ALTER COLUMN  codes_used_in_study DROP DEFAULT;
