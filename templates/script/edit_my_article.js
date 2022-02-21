b1926e53273ef61c8f84d6554d95fba44439d179
Michael Ajanaku <remiljw@gmail.com>
2022-02-21 11:43:55 +0100

    update changes on edit

--- templates/script/edit_my_article.js
+++ templates/script/edit_my_article.js
@@ -78,30 +78,35 @@ jQuery(document).ready(function () {
 toggleRadioElementDetailsOnChange(
   "#t_articles_data_doi__row",
   "results_based_on_data",
+  "#t_articles_data_doi",
   [2]
 );
 
 toggleRadioElementDetailsOnChange(
   "#t_articles_scripts_doi__row",
   "scripts_used_for_result",
+  "#t_articles_scripts_doi",
   [2]
 );
 
 toggleRadioElementDetailsOnChange(
   "#t_articles_codes_doi__row",
   "codes_used_in_study",
+  "#t_articles_codes_doi",
   [2]
 );
 
 function toggleRadioElementDetailsOnChange(
   details_div_id,
   question_number,
+  input_id,
   indexes
 ) {
   let levelRadioItems = document.querySelectorAll(
     `input[name="${question_number}"]`
   );
   let details_div = document.querySelector(details_div_id);
+  let input = document.querySelector(input_id);
 
   if (levelRadioItems) {
     levelRadioItems.forEach((item) => {
@@ -112,6 +117,7 @@ function toggleRadioElementDetailsOnChange(
 
         item.onchange = function () {
           if (details_div) {
+            input.value = "";
             details_div.style.display = "none";
           }
         };
