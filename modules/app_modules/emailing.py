8683529e2f5ba95ef0c495cd6b0cb7b7b8c5a1bc
Michael Ajanaku <remiljw@gmail.com>
2022-02-28 20:02:29 +0100

    remove duplicate variable

--- modules/app_modules/emailing.py
+++ modules/app_modules/emailing.py
@@ -2339,7 +2339,6 @@ def create_reminder_for_reviewer_review_invitation_registered_user(session, auth
 
         mail_vars["r2r_url"] = r2r_url
         mail_vars["trackchanges_url"] = trackchanges_url
-        mail_vars["reviewDuration"] = reviewDuration
 
         mail_vars["parallelText"] = ""
         if parallelSubmissionAllowed:
