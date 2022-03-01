b173534cf5bf250759cae40aa92297951b18e39b
ofa <fambon@github.users>
2022-03-01 08:33:43 +0100

    set default review duration form field w/ pciRRactivated

--- controllers/recommender.py
+++ controllers/recommender.py
@@ -1706,6 +1706,9 @@ def email_for_new_reviewer():
         programmaticRR_invitation_text = pci_rr_vars["programmaticRR_invitation_text"]
         signedreview_invitation_text = pci_rr_vars["signedreview_invitation_text"]
 
+    default_review_duration = "Two weeks" if pciRRactivated else "Three weeks"
+    reviewDuration = default_review_duration.lower()
+
     hashtag_template = emailing_tools.getCorrectHashtag("#DefaultReviewInvitationNewUser", art)
     mail_template = emailing_tools.getMailTemplateHashtag(db, hashtag_template)
     default_subject = emailing_tools.replaceMailVars(mail_template["subject"], locals())
@@ -1715,8 +1718,9 @@ def email_for_new_reviewer():
 
     replyto = db(db.auth_user.id == recomm.recommender_id).select(db.auth_user.id, db.auth_user.first_name, db.auth_user.last_name, db.auth_user.email).last()
     replyto_address = "%s, %s" % (replyto.email, myconf.take("contacts.managers"))
+
     form = SQLFORM.factory(
-        Field("review_duration", type="string", label=T("Select review duration"), default="Two weeks", writable=True, requires=IS_IN_SET(("Two weeks", "Three weeks", "Four weeks", "Five weeks", "Six weeks", "Seven weeks", "Eight weeks"))),
+        Field("review_duration", type="string", label=T("Select review duration"), default=default_review_duration, writable=True, requires=IS_IN_SET(("Two weeks", "Three weeks", "Four weeks", "Five weeks", "Six weeks", "Seven weeks", "Eight weeks"))),
         Field("replyto", label=T("Reply-to"), type="string", length=250, requires=IS_EMAIL(error_message=T("invalid e-mail!")), default=replyto_address, writable=False),
         Field.CC(default=(replyto.email, myconf.take("contacts.managers"))),
         Field("reviewer_first_name", label=T("Reviewer first name"), type="string", length=250, required=True),
