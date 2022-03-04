01223ef0344b40dad177f6ff3d4aba5a64634e06
ofa <fambon@github.users>
2022-03-04 13:52:33 +0100

    remove updates/*.sql

--- updates/add_new_field_to_reviews.sql
+++
@@ -1,4 +0,0 @@
-CREATE TYPE duration AS ENUM('Two weeks', 'Three weeks', 'Four weeks', 'Five weeks', 'Six weeks', 'Seven weeks', 'Eight weeks');
-
-ALTER TABLE "t_reviews"
-ADD COLUMN  IF NOT EXISTS review_duration duration DEFAULT 'Three weeks';
--- updates/delete_prev_field_mail_queue.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "mail_queue"
-DROP COLUMN  IF EXISTS reminder_soon_due,
-DROP COLUMN  IF EXISTS reminder_due,
-DROP COLUMN  IF EXISTS reminder_over_due;
