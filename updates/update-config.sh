8d9d98f355b6208c7ba63e787033d0d421779ff0
ofa <fambon@github.users>
2022-03-04 16:02:23 +0100

    update update-config.sh to touch reminders.py

--- updates/update-config.sh
+++ updates/update-config.sh
@@ -2,7 +2,7 @@ update_reminders_config() {
 	diff -u private/reminders_config private/sample.reminders_config
 
 	sed -i '/ReminderReviewerReview.*Due/ d' private/reminders_config
-	touch modules/app_modules/emailing_tools.py
+	touch modules/app_modules/reminders.py
 }
 
 set -x
