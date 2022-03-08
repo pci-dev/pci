c2ef7e8f20443ad8e962bbf329f11ed5209d6d9f
ofa <fambon@github.users>
2022-03-08 09:51:16 +0100

    add fix (replace) submitterName => articleAuthors in db updates/

--- updates/fix_appLongname_in_templates.sql
+++ updates/fix_appLongname_in_templates.sql
@@ -1,1 +1,2 @@
 UPDATE mail_templates SET contents = replace(contents, '{{appLongname}}', '{{appLongName}}');
+UPDATE mail_templates SET contents = replace(contents, '{{submitterName}}', '{{articleAuthors}}');
