71d261510f5c9a6f5c1d79feec6af2a3d82b8634
ofa <fambon@github.users>
2022-03-08 18:11:58 +0100

    rename RR 'Scheduled' template ...RegisterUserStage1 => RegisteredUserStage1

--- updates/rename_template_DefaultReviewInvitationRegisterUser.sql
+++ updates/rename_template_DefaultReviewInvitationRegisterUser.sql
@@ -7,3 +7,6 @@ where hashtag = '#DefaultReviewInvitationRegisterUserStage1';
 update mail_templates
 set hashtag = '#DefaultReviewInvitationRegisteredUserStage2'
 where hashtag = '#DefaultReviewInvitationRegisterUserStage2';
+update mail_templates
+set hashtag = '#DefaultReviewInvitationRegisteredUserStage1ScheduledSubmission'
+where hashtag = '#DefaultReviewInvitationRegisterUserStage1ScheduledSubmission';
