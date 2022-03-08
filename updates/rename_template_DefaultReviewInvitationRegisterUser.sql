0cbb71545434cfebea2c3351a7662f28ae149a45
ofa <fambon@github.users>
2022-03-08 17:38:14 +0100

    rename RR templates ...RegisterUserStage1/2 => RegisteredUserStage1/2

--- updates/rename_template_DefaultReviewInvitationRegisterUser.sql
+++ updates/rename_template_DefaultReviewInvitationRegisterUser.sql
@@ -1,3 +1,9 @@
 update mail_templates
 set hashtag = '#DefaultReviewInvitationRegisteredUser'
 where hashtag = '#DefaultReviewInvitationRegisterUser';
+update mail_templates
+set hashtag = '#DefaultReviewInvitationRegisteredUserStage1'
+where hashtag = '#DefaultReviewInvitationRegisterUserStage1';
+update mail_templates
+set hashtag = '#DefaultReviewInvitationRegisteredUserStage2'
+where hashtag = '#DefaultReviewInvitationRegisterUserStage2';
