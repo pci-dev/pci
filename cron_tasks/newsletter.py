6e0152aca0810d6fbe4560955a4f4479630b3714
ofa <fambon@github.users>
2022-03-15 12:34:39 +0100

    refactor newsletter.py w/o copy/paste

--- cron_tasks/newsletter.py
+++ cron_tasks/newsletter.py
@@ -5,57 +5,36 @@ from datetime import date, datetime, timedelta
 from app_modules import emailing
 
 
-conditions = ["client" not in request, auth.has_membership(role="manager")]
-if any(conditions):
+news = {
+    "Weekly": 7,
+    "Every two weeks": 14,
+    "Monthly": 30,
+}
+
+
+def send_newsletter(freq):
     my_date = datetime.now()
 
-    # Weekly newsletter
-    weekly_newsletter_date = my_date - timedelta(days=7)
-    users_with_weekly_newsletter = db(
+    newsletter_date = my_date - timedelta(days=news[freq])
+    users_with_newsletter = db(
         (
-            ((db.auth_user.last_alert != None) & (weekly_newsletter_date >= db.auth_user.last_alert))
-            | ((db.auth_user.last_alert == None) & (weekly_newsletter_date >= db.auth_user.registration_datetime))
+            ((db.auth_user.last_alert != None) & (newsletter_date >= db.auth_user.last_alert))
+          | ((db.auth_user.last_alert == None) & (newsletter_date >= db.auth_user.registration_datetime))
         )
-        & db.auth_user.alerts.contains("Weekly")
+        & db.auth_user.alerts.contains(freq)
     ).select()
 
-    for user in users_with_weekly_newsletter:
-        print("Weekly newsletter: " + user.first_name + " " + user.last_name)
+    for user in users_with_newsletter:
+        print(freq + " newsletter: " + user.first_name + " " + user.last_name)
         emailing.delete_newsletter_mail(session, auth, db, user.id)
-        emailing.send_newsletter_mail(session, auth, db, user.id, "Weekly")
+        emailing.send_newsletter_mail(session, auth, db, user.id, freq)
         user.last_alert = datetime.now()
         user.update_record()
 
-    # Two weeks newsletter
-    two_weeks_newsletter_date = my_date - timedelta(days=14)
-    users_with_two_weeks_newsletter = db(
-        (
-            ((db.auth_user.last_alert != None) & (two_weeks_newsletter_date >= db.auth_user.last_alert))
-            | ((db.auth_user.last_alert == None) & (two_weeks_newsletter_date >= db.auth_user.registration_datetime))
-        )
-        & db.auth_user.alerts.contains("Every two weeks")
-    ).select()
 
-    for user in users_with_two_weeks_newsletter:
-        print("Every two weeks newsletter: " + user.first_name + " " + user.last_name)
-        emailing.delete_newsletter_mail(session, auth, db, user.id)
-        emailing.send_newsletter_mail(session, auth, db, user.id, "Every two weeks")
-        user.last_alert = datetime.now()
-        user.update_record()
+def main():
+    for freq in news:
+        send_newsletter(freq)
 
-    # Monthly newsletter
-    monthly_newsletter_date = my_date - timedelta(days=30)
-    users_with_monthly_newsletter = db(
-        (
-            ((db.auth_user.last_alert != None) & (monthly_newsletter_date >= db.auth_user.last_alert))
-            | ((db.auth_user.last_alert == None) & (monthly_newsletter_date >= db.auth_user.registration_datetime))
-        )
-        & db.auth_user.alerts.contains("Monthly")
-    ).select()
 
-    for user in users_with_monthly_newsletter:
-        print("Monthly newsletter: " + user.first_name + " " + user.last_name)
-        emailing.delete_newsletter_mail(session, auth, db, user.id)
-        emailing.send_newsletter_mail(session, auth, db, user.id, "Monthly")
-        user.last_alert = datetime.now()
-        user.update_record()
+main()
