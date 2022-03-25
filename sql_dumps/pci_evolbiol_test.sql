2d9a5dcbdc73907693662b11beca25d09a95a18b
ofa <fambon@github.users>
2022-03-25 17:06:33 +0100

    remove updates/sql_dumps in-db checkboxes

--- sql_dumps/pci_evolbiol_test.sql
+++ sql_dumps/pci_evolbiol_test.sql
@@ -2934,15 +2934,3 @@ ADD COLUMN  IF NOT EXISTS suggest_reviewers text;
 
 ALTER TABLE auth_user
 ALTER alerts SET DEFAULT 'Weekly';
-
--- 2022-03-24 updates/update-submission.sql
-
-ALTER TABLE "t_articles"
-ADD COLUMN IF NOT EXISTS guide_read boolean NOT NULL DEFAULT true,
-ADD COLUMN IF NOT EXISTS approvals_obtained boolean NOT NULL DEFAULT true,
-ADD COLUMN IF NOT EXISTS human_subject_consent_obtained boolean NOT NULL DEFAULT true,
-ADD COLUMN IF NOT EXISTS lines_numbered boolean NOT NULL DEFAULT true,
-ADD COLUMN IF NOT EXISTS funding_sources_listed boolean NOT NULL DEFAULT true,
-ADD COLUMN IF NOT EXISTS conflicts_of_interest_indicated boolean NOT NULL DEFAULT true,
-ADD COLUMN IF NOT EXISTS no_financial_conflict_of_interest boolean NOT NULL DEFAULT true;
-
