127c04880701745469d51d52c273c12c314bf39c
Michael Ajanaku <remiljw@gmail.com>
2022-05-20 16:22:41 +0100

    update t_articles field

--- models/db.py
+++ models/db.py
@@ -657,7 +657,7 @@ db.define_table(
 
     Field("scheduled_submission_date", type="date", label=T("Scheduled submission date"), requires=IS_EMPTY_OR(IS_DATE(format=T('%Y-%m-%d'), error_message='must be a valid date: YYYY-MM-DD'))),
     Field("auto_nb_recommendations", type="integer", label=T("Rounds of reviews")),
-    Field("article_author", type="text", length=512, label=T("Article author"), readable=False, writable=False),
+    Field("article_submitter", type="text", length=512, label=T("Article submitter"), readable=False, writable=False),
     format="%(title)s (%(authors)s)",
     singular=T("Article"),
     plural=T("Articles"),
