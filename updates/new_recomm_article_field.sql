94899bf7d60714b94485584a3747929f13710642
Michael Ajanaku <remiljw@gmail.com>
2022-05-20 16:17:03 +0100

    update sql script

--- updates/new_recomm_article_field.sql
+++ updates/new_recomm_article_field.sql
@@ -1,5 +1,5 @@
 ALTER TABLE "t_articles"
-ADD COLUMN IF NOT EXISTS  article_author character varying(512);
+ADD COLUMN IF NOT EXISTS  article_submitter character varying(512);
 
 ALTER TABLE "t_recommendations"
 ADD COLUMN IF NOT EXISTS  recommender_details character varying(512);
