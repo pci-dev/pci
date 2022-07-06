6d303fbe1ca06c7a75b1086bd228f65ff9851487
ofa <fambon@github.users>
2022-07-06 19:05:17 +0200

    fup: add state descriptions in updates/..._article_states.sql

--- updates/add_scheduled_submission_article_states.sql
+++ updates/add_scheduled_submission_article_states.sql
@@ -1,4 +1,4 @@
 INSERT INTO t_status_article("status","color_class","explaination","priority_level")
 VALUES
-(E'Scheduled submission pending',E'danger',NULL,E'A'),
-(E'Scheduled submission under consideration',E'info',NULL,E'A');
+(E'Scheduled submission pending',E'danger',E'Scheduled submission waiting for Recommender approval',E'A'),
+(E'Scheduled submission under consideration',E'info',E'Scheduled submission accepted by Recommender',E'A');
