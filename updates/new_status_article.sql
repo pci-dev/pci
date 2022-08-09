388d0d1cd273019a36382ed405eb60ff6b80ae04
Michael Ajanaku <remiljw@gmail.com>
2022-08-09 12:45:58 +0100

    add sql scripts

---
+++ updates/new_status_article.sql
@@ -0,0 +1,3 @@
+INSERT INTO t_status_article("status","color_class","explaination","priority_level")
+VALUES
+(E'Pre-submission',E'danger',E'Submission needs changes from author',E'A');
---
+++ updates/pre_submission_help_text.sql
@@ -0,0 +1,5 @@
+INSERT INTO help_texts("hashtag","lang","contents")
+VALUES
+(E'#ManagePresubmittedArticles',E'default',E''),
+(E'#ManagePresubmittedArticlesText',E'default',E''),
+(E'#ManagePresubmittedArticlesTitle',E'default',E'Presubmitted Articles needing changes');
