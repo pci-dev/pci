0cb7aed9ed4341d50a34a2d28e0e8f09641bb82b
Michael Ajanaku <remiljw@gmail.com>
2022-08-19 18:42:43 +0100

    add sql file

---
+++ updates/pending_survey_help_text.sql
@@ -0,0 +1,5 @@
+INSERT INTO help_texts("hashtag","lang","contents")
+VALUES
+(E'#ManagerPendingSurveyReports',E'default',E''),
+(E'#ManagerPendingSurveyReportsText',E'default',E''),
+(E'#ManagerPendingSurveyReportsTitle',E'default',E'Report(s) without surveys');
