4e0805593f7212151788817475605238f3cf194f
ofa <fambon@github.users>
2022-08-26 16:45:12 +0200

    add new mail template

--- sql_dumps/insert_default_mail_templates.sql
+++ sql_dumps/insert_default_mail_templates.sql
@@ -186,3 +186,14 @@ values
 <p>Yours sincerely,</p>
 <p>The Managing Board of {{appLongName}}</p>
 ');
+
+-- 2022-08-26 updates/add_new_mail_hashtag.sql
+INSERT INTO "mail_templates"("hashtag","lang","subject","description","contents")
+VALUES
+(
+E'#SubmitterGenericMail',
+E'default',
+E'{{appName}}: Update on your submission',
+E'Generic mail to submitters to fix errors in their submission',
+E'<p>Dear {{destPerson}},</p>\n<p>Regarding the submission of your report entitled <strong>{{articleTitle}}</strong>,<br><br><br><b><em>**You can edit/write your message to the referee**</em></b><br><br><br></p><p>Thanks again for this submission.</p>\n<p>The managing board of {{appName}}</p>'
+);
