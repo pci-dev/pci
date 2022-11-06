pci-dev/pci

commit 30f5a35b8e1aab79a85b93dfed2ce7af11888821

enable markdown for article title

modules/app_modules/common_small_html.py
@@ -522,11 +522,11 @@ def mkRepresentArticleLightLinked(auth, db, article_id, urlArticle=None):
 
         if urlArticle:
             anchor = DIV(
-                A(B(art.title), _href=urlArticle), BR(), SPAN(mkAnonymousArticleField(auth, db, art.anonymous_submission, art.authors)), BR(), doi_text, _class="ellipsis-over-350",
+                A(B(WIKI(art.title)), _href=urlArticle), BR(), SPAN(mkAnonymousArticleField(auth, db, art.anonymous_submission, art.authors)), BR(), doi_text, _class="ellipsis-over-350",
             )
         else:
             anchor = DIV(
-                B(art.title or ""),
+                B(WIKI(art.title) or ""),
                 SPAN(mkAnonymousArticleField(auth, db, art.anonymous_submission, art.authors)),
                 BR(),
                 doi_text,
@@ -548,7 +548,7 @@ def mkRepresentArticleLightLinkedWithStatus(auth, db, article_id, urlArticle=Non
 
         if urlArticle:
             anchor = DIV(
-                A(B(art.title or "", _class="article-title"), _href=urlArticle),
+                A(B(WIKI(article.title) or "", _class="article-title"), _href=urlArticle),
                 BR(),
                 SPAN(mkAnonymousArticleField(auth, db, art.anonymous_submission, art.authors)),
                 BR(),
@@ -558,7 +558,7 @@ def mkRepresentArticleLightLinkedWithStatus(auth, db, article_id, urlArticle=Non
             )
         else:
             anchor = DIV(
-                B(art.title or ""),
+                B(WIKI(article.title) or ""),
                 SPAN(mkAnonymousArticleField(auth, db, art.anonymous_submission, art.authors)),
                 BR(),
                 doi_text,
@@ -579,7 +579,7 @@ def mkRepresentArticleLight(auth, db, article_id):
             doi_text = SPAN(B("Scheduled submission: ", _style="color: #ffbf00"), B(I(str(art.scheduled_submission_date))))
 
         anchor = DIV(
-            B(art.title, _class="article-title"),
+            B(WIKI(art.title), _class="article-title"),
             DIV(mkAnonymousArticleField(auth, db, art.anonymous_submission, art.authors)),
             doi_text,
             BR(),
@@ -605,7 +605,7 @@ def mkArticleCellNoRecomm(auth, db, art0):
             doi_text = SPAN(B("Scheduled submission: ", _style="color: #ffbf00"), B(I(str(art.scheduled_submission_date))))
 
         anchor = DIV(
-            B(art.title or "", _class="article-title"),
+            B(WIKI(art.title) or "", _class="article-title"),
             DIV(mkAnonymousArticleField(auth, db, art.anonymous_submission, art.authors)),
             doi_text,
             BR(),
@@ -648,7 +648,7 @@ def mkArticleCellNoRecommFromId(auth, db, recommId):
                 mkDOI(recomm.recommendation_doi),
                 P(),
                 SPAN(current.T("A recommendation of ")),
-                I(art.title or "", _class="article-title"),
+                I(WIKI(art.title) or "", _class="article-title"),
                 SPAN(current.T(" by ")),
                 SPAN(mkAnonymousArticleField(auth, db, art.anonymous_submission, art.authors)),
                 (SPAN(current.T(" in ")) + SPAN(art.article_source) if art.article_source else ""),
@@ -693,7 +693,7 @@ def mkArticleCitation(auth, db, myRecomm):
         citeArticle = SPAN(
             SPAN(art.authors),
             " ",
-            SPAN(art.title or "", _class="article-title"),
+            SPAN(WIKI(art.title) or "", _class="article-title"),
             ". ",
             SPAN(art.last_status_change.strftime("%Y, ")),
             artSrc,

controllers/manager.py
@@ -1509,7 +1509,7 @@ def send_submitter_generic_mail():
     # template variables, along with all other locals()
     destPerson = common_small_html.mkUser(auth, db, art.user_id)
     articleDoi = common_small_html.mkLinkDOI(art.doi)
-    articleTitle = art.title
+    articleTitle = WIKI(art.title)
     articleAuthors = "[undisclosed]" if (art.anonymous_submission) else art.authors
 
     default_subject = emailing_tools.replaceMailVars(mail_template["subject"], locals())

controllers/recommender.py
@@ -1339,7 +1339,7 @@ def send_review_cancellation():
     appName = myconf.take("app.name")
     contact = myconf.take("contacts.managers")
     art_authors = "[undisclosed]" if (art.anonymous_submission) else art.authors
-    art_title = art.title
+    art_title = WIKI(art.title)
     art_doi = common_small_html.mkLinkDOI(recomm.doi or art.doi)
     # art_doi = (recomm.doi or art.doi)
     linkTarget = None  # URL(c='user', f='my_reviews', vars=dict(pendingOnly=True), scheme=scheme, host=host, port=port)
@@ -1442,7 +1442,7 @@ def send_reviewer_generic_mail():
     destPerson = common_small_html.mkUser(auth, db, review.reviewer_id)
     recommenderPerson = common_small_html.mkUser(auth, db, auth.user_id)
     articleDoi = common_small_html.mkLinkDOI(recomm.doi or art.doi)
-    articleTitle = art.title
+    articleTitle = WIKI(art.title)
     articleAuthors = "[undisclosed]" if (art.anonymous_submission) else art.authors
 
     default_subject = emailing_tools.replaceMailVars(mail_template["subject"], locals())
@@ -1541,7 +1541,7 @@ def email_for_registered_reviewer():
     appLongName = myconf.take("app.longname")
     appName = myconf.take("app.name")
     art_authors = "[undisclosed]" if (art.anonymous_submission) else art.authors
-    art_title = art.title
+    art_title = WIKI(art.title)
     art_doi = common_small_html.mkLinkDOI(recomm.doi or art.doi)
     articleAuthors = art_authors
     articleTitle = art_title
@@ -1698,7 +1698,7 @@ def email_for_new_reviewer():
     port = myconf.take("alerts.port", cast=lambda v: common_tools.takePort(v))
     site_url = URL(c="default", f="index", scheme=scheme, host=host, port=port)
     art_authors = "[Undisclosed]" if (art.anonymous_submission) else art.authors
-    art_title = art.title
+    art_title = WIKI(art.title)
     art_doi = common_small_html.mkLinkDOI(recomm.doi or art.doi)
     articleAuthors = art_authors
     articleTitle = art_title

modules/app_components/article_components.py
@@ -83,7 +83,7 @@ def getRecommArticleRowCard(auth, db, response, article, withImg=True, withScore
     componentVars = dict(
         articleDate=date,
         articleUrl=URL(c="articles", f="rec", vars=dict(id=article.id), scheme=scheme, host=host, port=port),
-        articleTitle=article.title,
+        articleTitle=WIKI(article.title),
         articleImg=articleImg,
         isAlreadyPublished=article.already_published,
         articleAuthor=authors,
@@ -116,7 +116,7 @@ def getArticleTrackcRowCard(auth, db, response, article):
         
         firstDate = article.upload_timestamp.strftime(DEFAULT_DATE_FORMAT)
         lastDate = article.last_status_change.strftime(DEFAULT_DATE_FORMAT)
-        title = article.title
+        title = WIKI(article.title)
         if article.anonymous_submission:
             authors = "[anonymous submission]"
         else:
@@ -249,7 +249,7 @@ def getArticleInfosCard(auth, db, response, article, printable,
             ("articleSource", I(article.article_source or "")),
             ("articleId", article.id),
             ("articleImg", article_img),
-            ("articleTitle", article.title or ""),
+            ("articleTitle", WIKI(article.title) or ""),
             ("articleAuthor", authors or ""),
             ("articleDoi", doi_text),
             ("doiButton", doi_button),

modules/app_components/public_recommendation.py
@@ -191,7 +191,7 @@ def getArticleAndFinalRecommendation(auth, db, response, art, finalRecomm, print
 
 ######################################################################################################################################################################
 def getRecommendationMetadata(auth, db, art, lastRecomm, pdfLink, citeNum, scheme, host, port):
-    desc = "A recommendation of: " + (art.authors or "") + " " + (art.title or "") + " " + (art.doi or "")
+    desc = "A recommendation of: " + (art.authors or "") + " " + (WIKI(art.title) or "") + " " + (art.doi or "")
     whoDidItMeta = common_small_html.getRecommAndReviewAuthors(auth, db, recomm=lastRecomm, with_reviewers=False, linked=False, as_list=True)
 
     # META headers

commit 5a783216a558d690c0dfa2d3ead6e7291e6dc516

fix typo article.title => art.title (introduced in 30f5a35b8)

modules/app_modules/common_small_html.py
@@ -548,7 +548,7 @@ def mkRepresentArticleLightLinkedWithStatus(auth, db, article_id, urlArticle=Non
 
         if urlArticle:
             anchor = DIV(
-                A(B(md_to_html(article.title) or "", _class="article-title"), _href=urlArticle),
+                A(B(md_to_html(art.title) or "", _class="article-title"), _href=urlArticle),
                 BR(),
                 SPAN(mkAnonymousArticleField(auth, db, art.anonymous_submission, art.authors)),
                 BR(),
@@ -558,7 +558,7 @@ def mkRepresentArticleLightLinkedWithStatus(auth, db, article_id, urlArticle=Non
             )
         else:
             anchor = DIV(
-                B(md_to_html(article.title) or ""),
+                B(md_to_html(art.title) or ""),
                 SPAN(mkAnonymousArticleField(auth, db, art.anonymous_submission, art.authors)),
                 BR(),
                 doi_text,