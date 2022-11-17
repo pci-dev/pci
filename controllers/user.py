9ffccfc3b293742b3ebd26e3fa0da84ecc0cff52
ofa <fambon@github.users>
2022-11-17 21:51:00 +0100

    delete old article picture on update in article edits

--- controllers/user.py
+++ controllers/user.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import os
 import re
 import copy
 from datetime import date
@@ -789,6 +790,9 @@ def edit_my_article():
         article_version = int(art.ms_version)
     except:
         article_version = art.ms_version
+
+    prev_picture = art.uploaded_picture
+
     # form.element(_type="submit")["_value"] = T("Save")
     def onvalidation(form):
         if not pciRRactivated:
@@ -797,6 +801,10 @@ def edit_my_article():
                     form.errors.ms_version = "New version number must be greater than or same as previous version number"
 
     if form.process(onvalidation=onvalidation).accepted:
+        if prev_picture and form.vars.uploaded_picture:
+            try: os.unlink(os.path.join(request.folder, "uploads", prev_picture))
+            except: pass
+
         response.flash = T("Article saved", lazy=False)
         article = db.t_articles[articleId]
         if article.status == "Pre-submission":
