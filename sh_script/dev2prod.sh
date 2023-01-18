2a768c290c28bbe53253b32f9ddeca2ba9ac2e21
ofa <fambon@github.users>
2023-01-18 17:40:13 +0100

    remove unused sh_script/

--- sh_script/dev2prod.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-find . -name \*.py -ls -exec sed -i~ -e 's/AppConfig(reload=True)/AppConfig(reload=False)/; s/track_changes(True)/track_changes(False)/' {} \;
-
--- sh_script/prod2dev.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-find . -name \*.py -ls -exec sed -i~ -e 's/AppConfig(reload=False)/AppConfig(reload=True)/; s/track_changes(False)/track_changes(True)/' {} \;
-
