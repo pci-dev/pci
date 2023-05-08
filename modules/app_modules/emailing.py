cc4c61e92759bb49bc056b9f6841248ccce0fcaf
Michael Ajanaku <remiljw@gmail.com>
2023-05-08 19:31:03 +0100

    update create_cancellation_for_reviewer func()

--- modules/app_modules/emailing.py
+++ modules/app_modules/emailing.py
@@ -3143,7 +3143,10 @@ def create_cancellation_for_reviewer(session, auth, db, reviewId):
     if pciRRactivated:
         mail_vars.update(getPCiRRScheduledSubmissionsVars(art))
 
-    hashtag_template = emailing_tools.getCorrectHashtag("#DefaultReviewCancellation", art)
+    template = "#DefaultReviewCancellation"
+    if review.review_state == "Awaiting review":
+        template = "#DefaultReviewAlreadyAcceptedCancellation"
+    hashtag_template = emailing_tools.getCorrectHashtag(template, art)
     if check_mail_queue(db, hashtag_template, reviewer.email, review.recommendation_id) is False:
         emailing_tools.insertMailInQueue(auth, db, hashtag_template, mail_vars, recomm.id, None, recomm.article_id)
         reports = emailing_tools.createMailReport(True, mail_vars["destPerson"], reports)
