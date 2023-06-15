e6042292604feff99d66e37750ac7baf7e5fbc51
ofa <fambon@github.users>
2023-06-15 10:31:48 +0200

    add try/except in hypothesis.get_url_from_doi()

--- modules/app_modules/hypothesis.py
+++ modules/app_modules/hypothesis.py
@@ -106,8 +106,11 @@ class Hypothesis:
 
 
     def get_url_from_doi(self) -> str:
-        response = HttpClient().get(self.__article.doi, None, allow_redirects=True)
-        return response.url
+        try:
+            response = HttpClient().get(self.__article.doi, None, allow_redirects=True)
+            return response.url
+        except:
+            return None
 
 
     def __get_PCI_group_id(self) -> str:
