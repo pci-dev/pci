49519ea96bbbce9be3da62c61bb5c38c7d346453
ofa <fambon@github.users>
2023-06-23 10:57:12 +0200

    use models.typing _() and _cast()

--- modules/models/review.py
+++ modules/models/review.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from enum import Enum
-from typing import Iterable, List, Union, cast
+from typing import Iterable, List, cast
+from models.typing import _cast, _
 from pydal.objects import Row, Rows
 from pydal import DAL
 
@@ -19,25 +20,25 @@ class Review(Row):
     id: int
     recommendation_id: int
     reviewer_id: int
-    review: Union[str, None]
-    last_change: Union[datetime, None]
-    is_closed: Union[bool, None]
-    anonymously: Union[bool, None]
-    review_state: Union[str, None]
-    no_conflict_of_interest: Union[bool, None]
-    review_pdf: Union[str, None]
-    review_pdf_data: Union[bytes, None]
-    acceptation_timestamp: Union[datetime, None]
-    emailing: Union[str, None]
-    quick_decline_key: Union[str, None]
-    reviewer_details: Union[str, None]
-    review_duration: Union[datetime, None]
-    anonymous_agreement: Union[bool, None]
+    review: _(str)
+    last_change: _(datetime)
+    is_closed: _(bool)
+    anonymously: _(bool)
+    review_state: _(str)
+    no_conflict_of_interest: _(bool)
+    review_pdf: _(str)
+    review_pdf_data: _(bytes)
+    acceptation_timestamp: _(datetime)
+    emailing: _(str)
+    quick_decline_key: _(str)
+    reviewer_details: _(str)
+    review_duration: _(datetime)
+    anonymous_agreement: _(bool)
 
 
     @staticmethod
     def get_by_id(db: DAL, id: int):
-        return cast(Union[Review, None], db.t_reviews[id])
+        return _cast(Review, db.t_reviews[id])
 
 
     @staticmethod
