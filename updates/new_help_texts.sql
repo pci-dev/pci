366028a3351aa1f64d680d0bc771cd902b10db97
Michael Ajanaku <remiljw@gmail.com>
2023-07-19 15:52:08 +0100

    add help texts

---
+++ updates/new_help_texts.sql
@@ -0,0 +1,45 @@
+INSERT INTO help_texts("hashtag","lang","contents")
+VALUES
+(E'#RecommenderStatistics',E'default',E''),
+(E'#RecommenderStatisticsText',E'default',E'<p><span style="line-height: 1.2em;"> This page lists  submissions handled by all recommenders on the platform.</span></p>\n<p><span style="line-height: 1.2em;">In case of any problems or difficulties, please contact us at <a href="mailto:?to={{contact}}">{{contact}}</a></span></p>'),
+(E'#RecommenderStatisticsTitle',E'default',E'Recommender Statistics'),
+
+(E'#TotalInvitationStats',E'default',E''),
+(E'#TotalInvitationStatsText',E'default',E'<p><span style="line-height: 1.2em;">Recommendation invites received by a recommender.</span></p>\n<p><span style="line-height: 1.2em;">In case of any problems or difficulties, please contact us at <a href="mailto:?to={{contact}}">{{contact}}</a></span></p>'),
+(E'#TotalInvitationStatsTitle',E'default',E'Total Invitations'),
+
+(E'#TotalAcceptedStats',E'default',E''),
+(E'#TotalAcceptedStatsText',E'default',E'<p><span style="line-height: 1.2em;">Recommendation invites accepted by a recommender.</span></p>\n<p><span style="line-height: 1.2em;">In case of any problems or difficulties, please contact us at <a href="mailto:?to={{contact}}">{{contact}}</a></span></p>'),
+(E'#TotalAcceptedStatsTitle',E'default',E'Total Accepted'),
+
+(E'#TotalCompletedStats',E'default',E''),
+(E'#TotalCompletedStatsText',E'default',E'<p><span style="line-height: 1.2em;">Recommendations completed by a recommender.</span></p>\n<p><span style="line-height: 1.2em;">In case of any problems or difficulties, please contact us at <a href="mailto:?to={{contact}}">{{contact}}</a></span></p>'),
+(E'#TotalCompletedStatsTitle',E'default',E'Total Completed'),
+
+(E'#CurrentInvitationStats',E'default',E''),
+(E'#CurrentInvitationStatsText',E'default',E'<p><span style="line-height: 1.2em;">Recommendation invites yet to be accepted by a recommender.</span></p>\n<p><span style="line-height: 1.2em;">In case of any problems or difficulties, please contact us at <a href="mailto:?to={{contact}}">{{contact}}</a></span></p>'),
+(E'#CurrentInvitationStatsTitle',E'default',E'Current Invitations'),
+
+(E'#CurrentAssignmentStats',E'default',E''),
+(E'#CurrentAssignmentStatsText',E'default',E'<p><span style="line-height: 1.2em;">Ongoing recommendations handled by a recommender.</span></p>\n<p><span style="line-height: 1.2em;">In case of any problems or difficulties, please contact us at <a href="mailto:?to={{contact}}">{{contact}}</a></span></p>'),
+(E'#CurrentAssignmentStatsTitle',E'default',E'Current Assignments'),
+
+(E'#AwaitingRevisionStats',E'default',E''),
+(E'#AwaitingRevisionStatsText',E'default',E'<p><span style="line-height: 1.2em;">Ongoing recommendations that have the status “Awaiting revision”.</span></p>\n<p><span style="line-height: 1.2em;">In case of any problems or difficulties, please contact us at <a href="mailto:?to={{contact}}">{{contact}}</a></span></p>'),
+(E'#AwaitingRevisionStatsTitle',E'default',E'Awaiting Revision'),
+
+(E'#RequiringActionStats',E'default',E''),
+(E'#RequiringActionStatsText',E'default',E'<p><span style="line-height: 1.2em;">Ongoing recommendations requiring attention.</span></p>\n<p><span style="line-height: 1.2em;">In case of any problems or difficulties, please contact us at <a href="mailto:?to={{contact}}">{{contact}}</a></span></p>'),
+(E'#RequiringActionStatsTitle',E'default',E'Requiring Actions'),
+
+(E'#RequiringReviewerStats',E'default',E''),
+(E'#RequiringReviewerStatsText',E'default',E'<p><span style="line-height: 1.2em;">Ongoing recommendations that are short of reviewers.</span></p>\n<p><span style="line-height: 1.2em;">In case of any problems or difficulties, please contact us at <a href="mailto:?to={{contact}}">{{contact}}</a></span></p>'),
+(E'#RequiringReviewerStatsTitle',E'default',E'Requiring Reviewers'),
+
+(E'#RequiredReviewsCompletedStats',E'default',E''),
+(E'#RequiredReviewsCompletedStatsText',E'default',E'<p><span style="line-height: 1.2em;">Ongoing recommendations for which all agreed reviews have been completed.</span></p>\n<p><span style="line-height: 1.2em;">In case of any problems or difficulties, please contact us at <a href="mailto:?to={{contact}}">{{contact}}</a></span></p>'),
+(E'#RequiredReviewsCompletedStatsTitle',E'default',E'Required Reviews Completed'),
+
+(E'#LateReviewStat',E'default',E''),
+(E'#LateReviewStatText',E'default',E'<p><span style="line-height: 1.2em;">Ongoing recommendations for which at least one agreed review is late. </span></p>\n<p><span style="line-height: 1.2em;">In case of any problems or difficulties, please contact us at <a href="mailto:?to={{contact}}">{{contact}}</a></span></p>'),
+(E'#LateReviewStatTitle',E'default',E'Late Reviews');
