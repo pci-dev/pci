42b0e8716d16cdbf7fead4a8fdb7958faeb06043
ofa <fambon@github.users>
2023-07-20 18:59:38 +0200

    add process_request(json) in coar_notify/inbox (initial)

--- controllers/coar_notify.py
+++ controllers/coar_notify.py
@@ -42,7 +42,6 @@ def index():
 def inbox():
     coar_notifier = COARNotifier(db)
 
-    # There is no inbox if COAR Notify is not enabled.
     if not coar_notifier.enabled:
         raise HTTP(status=http.HTTPStatus.NOT_FOUND.value)
 
@@ -54,43 +53,50 @@ def inbox():
             }
         )
         return ""
-    elif request.method == "GET":
-        # The Linked Data Notifications specification requires that every inbox supports
-        # GET, subject to access controls. As we don't support it, we instead say it is
-        # forbidden for everybody.
-        raise HTTP(status=http.HTTPStatus.FORBIDDEN.value)
-    elif request.method == "POST":
-        # The POST method is used for submitting notifications to the inbox.
 
+    elif request.method == "GET":
+        raise HTTP(status=http.HTTPStatus.FORBIDDEN.value)
+
+    elif request.method == "POST":
         if current.isRR: raise HTTP(status=http.HTTPStatus.FORBIDDEN.value)
 
         content_type, content_type_options = cgi.parse_header(
             request.env.content_type or ""
         )
         if content_type not in _rdflib_parser_media_types:
-            return HTTP(
+            raise HTTP(
                 http.HTTPStatus.UNSUPPORTED_MEDIA_TYPE.value,
                 f"Content-Type must be one of {', '.join(sorted(_rdflib_parser_media_types))})",
             )
 
+        body = request.body.read()
+        validate_request(body, content_type, coar_notifier)
+
+        process_request(json.loads(body))
+
+        return HTTP(status=http.HTTPStatus.ACCEPTED.value)
+
+    else:
+        raise HTTP(
+            http.HTTPStatus.METHOD_NOT_ALLOWED.value,
+            **{ "Allow": ", ".join(["POST", "OPTIONS"]) },
+        )
+
+
+def process_request(req):
+    print("origin:", req["origin"])
+
+
+def validate_request(body, content_type, coar_notifier):
         try:
             coar_notifier.record_notification(
-                body=request.body,
+                body=body,
                 body_format=_rdflib_parser_media_types[content_type],
                 direction="Inbound",
                 base=coar_notifier.base_url + 'coar_notify/inbox/',
             )
         except COARNotifyException as e:
             raise HTTP(status=http.HTTPStatus.BAD_REQUEST.value, body=e.message) from e
-        else:
-            raise HTTP(status=http.HTTPStatus.ACCEPTED.value, body='')
-    else:
-        raise HTTP(
-            http.HTTPStatus.METHOD_NOT_ALLOWED.value,
-            **{
-                "Allow": ", ".join(["POST", "OPTIONS"]),
-            },
-        )
 
 
 def show_coar_status():
