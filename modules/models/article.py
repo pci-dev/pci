c1fc59ff6887ee3e2a9d2a4a0f5eb9783100155a
Charly <charly.maeder@cefe.cnrs.fr>
2023-07-31 17:01:59 +0200

    Add ArticleStatus enum.

--- modules/models/article.py
+++ modules/models/article.py
@@ -1,7 +1,19 @@
 from datetime import datetime
+from enum import Enum
 from typing import Optional as _, cast
 from pydal.objects import Row
 from pydal import DAL
+
+class ArticleStatus(Enum):
+    NOT_CONSIDERED = 'Not considered'
+    PRE_RECOMMENDED = 'Pre-recommended'
+    AWAITING_REVISION = 'Awaiting revision'
+    RECOMMENDED = 'Recommended'
+    UNDER_CONSIDERATION = 'Under consideration'
+    REJECTED = 'Rejected'
+    PRE_REJECTED = 'Pre-rejected'
+    CANCELLED = 'Cancelled'
+    AWAITING_CONSIDERATION = 'Awaiting consideration'
 
 
 class Article(Row):
