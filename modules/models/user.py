f4e244bd5c77008a64deefe1640184e59c22f223
Charly <charly.maeder@cefe.cnrs.fr>
2023-08-22 15:43:01 +0200

    Add is_profile_completed() in user model.

--- modules/models/user.py
+++ modules/models/user.py
@@ -1,5 +1,6 @@
+from __future__ import annotations # for self-ref param type Post in save_posts_in_db()
 from datetime import datetime
-from typing import Optional as _, cast
+from typing import List, Optional as _, cast
 from pydal.objects import Row
 from pydal import DAL
 
@@ -21,7 +22,7 @@ class User(Row):
     laboratory: _[str]
     institution: _[str]
     alerts: _[str]
-    thematics: _[str]
+    thematics: _[List[str]]
     cv: _[str]
     last_alert: _[datetime]
     registration_datetime: _[datetime]
@@ -41,3 +42,21 @@ class User(Row):
     def get_by_reset_password_key(db: DAL, reset_password_key: str):
         user = db(db.auth_user.reset_password_key == reset_password_key).select().first()
         return cast(_[User], user)
+
+
+    @staticmethod
+    def is_profile_completed(user: User): 
+        return user.first_name != None and len(user.first_name) > 0 \
+            and user.last_name != None and len(user.last_name) > 0 \
+            and user.email != None and len(user.email) > 0 \
+            and user.laboratory != None and len(user.laboratory) > 0 \
+            and user.institution != None and len(user.institution) > 0 \
+            and user.city != None and len(user.city) > 0 \
+            and user.country != None and len(user.country) > 0 \
+            and user.thematics != None and len(user.thematics) > 0 \
+            and user.cv != None and len(user.cv) > 0 \
+            and user.keywords != None and len(user.keywords) > 0 \
+            and user.website != None and len(user.website) > 0 \
+            and user.alerts != None and len(user.alerts) > 0
+
+
