4b72be88bc291f387bbf70d85123146b4f2887cc
Charly <charly.maeder@cefe.cnrs.fr>
2023-08-23 14:01:38 +0200

    Add handle of connection dialog in test.

--- tests/conftest.py
+++ tests/conftest.py
@@ -124,7 +124,10 @@ def login(user):
     select("#auth_user_email").send_keys(user.email)
     select("#auth_user_password").send_keys(user.password)
     select("input.btn").click()
-    select("#complete-profile-cancel-dialog").wait_clickable().click()
+    driver.implicitly_wait(3)
+    complete_profile_dialog = select("#complete-profile-cancel-dialog")
+    if complete_profile_dialog:
+        complete_profile_dialog.click()
     select.notif("Logged in").wait_clickable().click()
 
 def logout(user):
