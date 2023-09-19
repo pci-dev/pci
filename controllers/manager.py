80d1ca4ec5081cfe58a63e8e8d2f032213a52016
Michael Ajanaku <remiljw@gmail.com>
2023-09-19 22:10:10 +0100

    increase pagination

--- controllers/manager.py
+++ controllers/manager.py
@@ -1520,6 +1520,7 @@ def recommender_statistics():
         create=False,
         searchable=False,
         csv=False,
+        paginate=1000,
         maxtextlength=256,
         orderby=~db.v_recommender_stats.id,
         fields=[
