676fb78d7e5f14471dd9c7c96c36836f1642ba8b
Michael Ajanaku <remiljw@gmail.com>
2023-09-22 13:13:46 +0100

    correct query for "requiring_reviewers"

--- updates/views_RR-only.sql
+++ updates/views_RR-only.sql
@@ -33,11 +33,31 @@ SELECT
     (SELECT COUNT(recomm.id) FROM t_articles art, t_recommendations recomm, v_article_recommender v_art WHERE art.id=recomm.article_id and art.status in ('Under consideration', 'Awaiting revision', 'Scheduled submission revision', 'Scheduled submission under consideration') and art.report_stage in ('STAGE 1', 'STAGE 2') and recomm.recommender_id=recommender.id and recomm.id = v_art.recommendation_id) AS current_assignments,
     (SELECT COUNT(recomm.id) FROM t_articles art, t_recommendations recomm, v_article_recommender v_art WHERE art.id=recomm.article_id and art.status in ('Awaiting revision', 'Scheduled submission revision') and art.report_stage in ('STAGE 1', 'STAGE 2') and  recomm.recommender_id=recommender.id and recomm.id = v_art.recommendation_id) AS awaiting_revision,
     (SELECT COUNT(DISTINCT id) FROM (
-        SELECT DISTINCT recomm.id FROM t_articles art, t_reviews trev, t_recommendations recomm, v_article_recommender v_art WHERE recomm.article_id = art.id and trev.recommendation_id = recomm.id and art.status = 'Under consideration' and recomm.recommender_id=recommender.id and recomm.id=v_art.recommendation_id GROUP BY recomm.id, art.id, recomm.recommender_id HAVING (COUNT(trev.id) < 2 and art.report_stage = 'STAGE 1') or (COUNT(trev.id) = 0 and art.report_stage = 'STAGE 2')
-		    UNION ALL
+        SELECT DISTINCT recomm.id FROM t_articles art JOIN t_recommendations recomm ON art.id = recomm.article_id JOIN v_article_recommender v_art ON recomm.id = v_art.recommendation_id 
+        LEFT JOIN (
+                SELECT recommendation_id, COUNT(*) AS num_reviews
+                FROM t_reviews
+                GROUP BY recommendation_id
+        ) rev ON recomm.id = rev.recommendation_id
+        WHERE art.status = 'Under consideration'
+            AND recomm.recommender_id = recommender.id
+        GROUP BY recomm.id, art.report_stage, rev.num_reviews
+        HAVING ((art.report_stage = 'STAGE 1' AND COALESCE(rev.num_reviews, 0) < 2) OR (art.report_stage = 'STAGE 2' AND COALESCE(rev.num_reviews, 0) = 0))
+            UNION ALL
 	    SELECT DISTINCT recomm.id FROM t_articles art JOIN t_recommendations recomm ON recomm.article_id = art.id JOIN t_reviews trev ON trev.recommendation_id = recomm.id JOIN v_article_recommender v_art ON recomm.id = v_art.recommendation_id LEFT JOIN t_reviews trew ON trew.recommendation_id = recomm.id AND trew.review_state = 'Awaiting review' WHERE art.status = 'Under consideration' AND recomm.recommender_id = recommender.id AND trev.review_state = 'Review completed' 
             GROUP BY recomm.id, art.id, recomm.recommender_id HAVING COUNT(trev.id) >= 2 AND SUM(CASE WHEN trew.id IS NULL THEN 0 ELSE 1 END) = 0) requiring_action_nb) AS requiring_action,
-    (SELECT COALESCE(SUM(nb), 0) FROM ((SELECT COUNT(*) AS nb FROM t_articles art, t_reviews trev, t_recommendations recomm, v_article_recommender v_art WHERE recomm.article_id = art.id and trev.recommendation_id = recomm.id  and art.status = 'Under consideration' and recomm.recommender_id=recommender.id and recomm.id=v_art.recommendation_id GROUP BY art.id, recomm.recommender_id HAVING (COUNT(trev.id) < 2 and art.report_stage = 'STAGE 1') or (COUNT(trev.id) = 0 and art.report_stage = 'STAGE 2'))) subquery) AS requiring_reviewers,
+    (SELECT COALESCE(SUM(nb), 0) 
+    FROM (SELECT COUNT(*) AS nb FROM t_articles art JOIN t_recommendations recomm ON art.id = recomm.article_id JOIN v_article_recommender v_art ON recomm.id = v_art.recommendation_id 
+        LEFT JOIN (
+                SELECT recommendation_id, COUNT(*) AS num_reviews
+                FROM t_reviews
+                GROUP BY recommendation_id
+        ) rev ON recomm.id = rev.recommendation_id
+        WHERE art.status = 'Under consideration'
+            AND recomm.recommender_id = recommender.id
+        GROUP BY recomm.id, art.report_stage, rev.num_reviews
+        HAVING ((art.report_stage = 'STAGE 1' AND COALESCE(rev.num_reviews, 0) < 2) OR (art.report_stage = 'STAGE 2' AND COALESCE(rev.num_reviews, 0) = 0))
+    ) subquery) AS requiring_reviewers,
     (SELECT COALESCE(SUM(nb), 0) FROM (SELECT COUNT(DISTINCT recomm.id) AS nb FROM t_articles art JOIN t_recommendations recomm ON recomm.article_id = art.id JOIN t_reviews trev ON trev.recommendation_id = recomm.id JOIN v_article_recommender v_art ON recomm.id = v_art.recommendation_id LEFT JOIN t_reviews trew ON trew.recommendation_id = recomm.id AND trew.review_state = 'Awaiting review' WHERE art.status = 'Under consideration' AND recomm.recommender_id = recommender.id AND trev.review_state = 'Review completed' 
             GROUP BY recomm.id, art.id, recomm.recommender_id HAVING COUNT(trev.id) >= 2 AND SUM(CASE WHEN trew.id IS NULL THEN 0 ELSE 1 END) = 0) subquery) AS required_reviews_completed,
     (SELECT COUNT(DISTINCT recomm.id) FROM t_articles art, t_reviews trev, t_recommendations recomm, v_article_recommender v_art WHERE recomm.article_id = art.id and trev.recommendation_id = recomm.id and recomm.recommender_id=recommender.id and recomm.id=v_art.recommendation_id and recomm.recommendation_state = 'Ongoing' and trev.review_state = 'Awaiting review' and trev.acceptation_timestamp + convert_duration_to_sql_interval(trev.review_duration) < NOW()) AS late_reviews
