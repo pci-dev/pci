710c9c15f26336256335ba8575891ad22a964ace
ofa <fambon@github.users>
2023-10-04 08:48:37 +0200

    set default pdf_max_size = 10

--- models/db.py
+++ models/db.py
@@ -48,7 +48,7 @@ appName = myconf.take("app.name")
 scheme = myconf.take("alerts.scheme")
 host = myconf.take("alerts.host")
 port = myconf.take("alerts.port", cast=lambda v: common_tools.takePort(v))
-pdf_max_size = int(myconf.take("config.pdf_max_size") or 5)
+pdf_max_size = int(myconf.take("config.pdf_max_size") or 10)
 
 scheduledSubmissionActivated = myconf.get("config.scheduled_submissions", default=False)
 pciRRactivated = myconf.get("config.registered_reports", default=False)
