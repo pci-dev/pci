ef23b41c49e09e1abc53d8c384c26f3082b6171c
Michael Ajanaku <remiljw@gmail.com>
2023-10-11 15:48:08 +0100

    increase matched result

--- modules/app_modules/helper.py
+++ modules/app_modules/helper.py
@@ -190,9 +190,8 @@ def query_semantic_api(authors: list, recommenders: list):
                     paper_url = f'https://api.semanticscholar.org/{matched_paper["paperId"]}'
                     grid.append(DIV(SPAN(f'{author["name"]} has co-published with {data["group"]} {data["name"]} on this ', A('paper', _href=paper_url, _target="_blank"), f' published in {matched_paper["year"]}')))
                     grid.append(BR())
-                    break
         if not found:
-            grid.append(DIV(SPAN(nome, BR(), f'{author["name"]} has not co-published with any suggested recommenders')))
+            grid.append(DIV(SPAN(nome, BR(), f'{author["name"]} has not co-published with anyone assigned to this submission.')))
             grid.append(BR())
     return grid
 
