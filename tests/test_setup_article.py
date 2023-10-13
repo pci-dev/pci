58667af8c46007626099619b4d9ee769cbd1d38f
ofa <fambon@github.users>
2023-10-13 15:49:02 +0200

    remove commented-out code

--- tests/test_setup_article.py
+++ tests/test_setup_article.py
@@ -197,16 +197,6 @@ class Recommender_handles:
     notif.contains("e-mail sent to submitter")
     notif.contains("e-mail sent to " + recommender.name)
 
-#  def search_and_invite_registered_reviewer(_):
-#     select(".btn", contains="Choose a reviewer from the".upper()).click()
-#     select('#simple-search-input').send_keys(reviewer.name + "\n")
-
-#     select("a", "Prepare an invitation".upper()).click()
-#     select("input[type=submit]").click()
-
-#     select.notif(contains="e-mail sent to " + reviewer.name)
-#     select("a", "Done".upper()).click()
-
  def invite_external_unregistered_reviewer(_):
     select(".dropdown-toggle", contains="For recommenders").click()
     select("a", f"{preprint.capitalize()}(s) you are handling").click()
