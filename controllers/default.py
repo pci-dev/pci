d19b5e97ad8e415c3cea21d792189be276d6652d
Charly <charly.maeder@cefe.cnrs.fr>
2023-10-18 16:02:53 +0200

    Set correct redirect URL for register page.

--- controllers/default.py
+++ controllers/default.py
@@ -252,7 +252,7 @@ def user():
             myBottomText = getText(request, auth, db, "#ProfileBottomText")
             db.auth_user.ethical_code_approved.requires = IS_IN_SET(["on"])
             form.element(_type="submit")["_class"] = "btn btn-success"
-            OrcidTools.add_orcid_auth_user_form(session, request, form, URL(c="default", f="user", args="profile", host=host, scheme=scheme, user_signature=True))
+            OrcidTools.add_orcid_auth_user_form(session, request, form, URL(c="default", f="user", args="register", host=host, scheme=scheme))
             form.element('#auth_user_password_two__label').components[0] = SPAN(T("Confirm Password")) + SPAN(" * ", _style="color:red;")
             if suite:
                 auth.settings.register_next = suite
