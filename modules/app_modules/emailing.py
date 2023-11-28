bf4d107183426b1ace395fa8efc8ec1d09a38213
Charly <charly.maeder@cefe.cnrs.fr>
2023-11-28 18:12:56 +0100

    Function to send UnsubscriptionManagerAlert to manager.

--- modules/app_modules/emailing.py
+++ modules/app_modules/emailing.py
@@ -3513,3 +3513,20 @@ def send_message_to_recommender_and_reviewers(auth, db, article_id):
         mail_vars["ccAddresses"] = recommenders_mails[1:]
 
         emailing_tools.insertMailInQueue(auth, db, hashtag_template, mail_vars, None, None, article.id)
+
+##################################################################################################################################################################
+
+def send_unsubscription_alert_for_manager(auth: Auth, db: DAL):
+    mail_vars = emailing_tools.getMailCommonVars()
+    hashtag_template = "#UnsubscriptionManagerAlert"
+
+    mail_vars["person"] = common_small_html.mkUser(auth, db, auth.user.id)
+    mail_vars["address"] = auth.user.email
+    
+    mail_vars["destAddress"] = mail_vars["appContactMail"]
+    mail_vars["ccAddresses"] = emailing_vars.getManagersMails(db)
+
+    emailing_tools.insertMailInQueue(auth, db, hashtag_template, mail_vars)
+
+
+
