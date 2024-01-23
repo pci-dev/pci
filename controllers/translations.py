551d6a82db9993b2e25c9602027bcd58b5271095
Charly <charly.maeder@cefe.cnrs.fr>
2024-01-23 15:11:17 +0100

    Add interface to save/delete/generate article translation.

--- controllers/translations.py
+++ controllers/translations.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from typing import List, cast
 from gluon import current
 from gluon.contrib.appconfig import AppConfig
@@ -10,6 +11,7 @@ from app_modules.article_translator import ArticleTranslator
 from models.article import Article, TranslatedFieldType, TranslatedFieldDict
 from pydal.base import DAL
 from app_modules import common_tools
+from app_modules import common_small_html
 
 db = cast(DAL, current.db)
 response = cast(Response, current.response)
@@ -17,6 +19,10 @@ request = cast(Request, current.request)
 session = cast(Session, current.session)
 auth = cast(Auth, current.auth)
 config = AppConfig()
+
+scheme = config.take("alerts.scheme")
+host = config.take("alerts.host")
+port = config.take("alerts.port", cast=lambda v: common_tools.takePort(v))
 
 
 @auth.requires_signature()
@@ -52,39 +58,88 @@ def edit_article_translations():
     return dict(
             form=form,
             myFinalScript=common_tools.get_script("translations.js"),
-            pageTitle=english_field.capitalize() + "'s translations"
+            pageTitle=english_field.capitalize() + "'s translations",
+            confirmationScript = common_small_html.confirmationDialog('Are you sure?')
         )
 
 
 @auth.requires_login()
-def add_new_language():
+def add_or_edit_article_field_translation():
+    article_id = int(request.vars["article_id"])
+    translated_field = TranslatedFieldType(request.vars["field"])
+    lang = Lang.get_lang_by_code(request.vars["lang"])
+    action = AddNewLanguageAction(request.vars["action"])
+    translation = str(request.vars["translation"] or '')
+
+    if not translated_field:
+        response.flash = "Translated field not found"
+        return
+
+    article = Article.get_by_id(db, article_id)
+    if not article:
+        response.flash = "Article not found"
+        return
+    
+    if article.user_id != auth.user_id and not auth.has_membership(role="manager"):
+        response.flash = "Forbidden"
+        return
+    
+    is_textarea = translated_field == TranslatedFieldType.ABSTRACT
+    exists_translation = translation_value = Article.get_translation(article, translated_field, lang) != None
+
+    if action == AddNewLanguageAction.GENERATE:
+        try:
+            ArticleTranslator(lang, article).run_field_article_translation(translated_field)
+        except Exception:
+            response.flash = f"Automatic translation not possible: {lang.value.english_name} not supported."
+            return
+    else:
+        if len(translation) == 0:
+            response.flash = 'Text input field empty'
+            return
+        
+        new_translation: TranslatedFieldDict = {
+            'automated': False,
+            'content': translation,
+            'lang': lang.value.code
+        }
+        Article.add_or_update_translation(article, translated_field, new_translation)
+
+    translation_value = Article.get_translation(article, translated_field, lang)
+
+    if not translation_value:
+        response.flash = f"No translation for {translated_field.value} in {lang.value.english_name}"
+        return
+
+    if exists_translation:
+        response.flash = f"Translation in {lang.value.english_name} updated"
+    else:
+        response.flash = f"Translation in {lang.value.english_name} added"
+
+    return _generate_lang_form(article, translated_field, translation_value, is_textarea)
+
+
+@auth.requires_login()
+def delete_translation():
     article_id = int(request.vars["article_id"])
     translated_field = TranslatedFieldType(request.vars["field"])
     lang = Lang.get_lang_by_code(request.vars["lang"])
 
     if not translated_field:
-        session.flash = "Translated field not found"
+        response.flash = "Translated field not found"
         return
 
     article = Article.get_by_id(db, article_id)
     if not article:
-        session.flash = "Article not found"
+        response.flash = "Article not found"
         return
     
     if article.user_id != auth.user_id and not auth.has_membership(role="manager"):
-        session.flash = "Forbidden"
+        response.flash = "Forbidden"
         return
     
-    is_textarea = translated_field == TranslatedFieldType.ABSTRACT
-
-    ArticleTranslator(lang, article).run_field_article_translation(translated_field)
-
-    translation_value = Article.get_translation(article, translated_field, lang)
-    if not translation_value:
-        session.flash = f"No translation for {translated_field.value} in {lang.value.english_name}"
-        return
-
-    return _generate_lang_form(translation_value, is_textarea)
+    Article.delete_translation(article, translated_field, lang)
+    response.flash = f"Translation in {lang.value.english_name} deleted"
 
 
 def _generate_form(article: Article, translated_field: TranslatedFieldType, english_field: str):
@@ -95,46 +150,62 @@ def _generate_form(article: Article, translated_field: TranslatedFieldType, engl
     form.append(_generate_english_form(article, english_field, is_textarea))
     form.append(HR())
     
-    form.append(_generate_lang_selector(article, translated_field))
+    form.append(_generate_lang_selector(article, translated_field, is_textarea))
     form.append(HR())
 
     lang_form: List[FORM] = []
     if translations_value:
         for translation_value in translations_value:
-            lang_form.append(_generate_lang_form(translation_value, is_textarea))
+            lang_form.append(_generate_lang_form(article, translated_field, translation_value, is_textarea))
     form.append(DIV(*lang_form, _id="lang-form-list"))
 
     return DIV(*form)
 
 
-def _generate_lang_selector(article: Article, translated_field: TranslatedFieldType):
+def _generate_lang_selector(article: Article, translated_field: TranslatedFieldType, is_textarea: bool):
     lang_list = [lang.value for lang in Lang if lang != DEFAULT_LANG]
     lang_list = sorted(lang_list, key=lambda lang: lang.english_name)
     lang_options = [OPTION(_build_lang_name(lang), _value=lang.code) for lang in lang_list]
     lang_options.insert(0, OPTION(''))
 
-    scheme = config.take("alerts.scheme")
-    host = config.take("alerts.host")
-    port = config.take("alerts.port", cast=lambda v: common_tools.takePort(v))
-
-    url = cast(str,
+    url_generate = cast(str,
                URL(c="translations",
-                   f="add_new_language",
-                   vars=dict(article_id=article.id, field=translated_field.value),
+                   f="add_or_edit_article_field_translation",
+                   vars=dict(article_id=article.id, field=translated_field.value, action=AddNewLanguageAction.GENERATE.value),
                    user_signature=True,
                    host=host,
                    scheme=scheme,
                    port=port)
                )
-
+    
+    url_write = cast(str,
+               URL(c="translations",
+                   f="add_or_edit_article_field_translation",
+                   vars=dict(article_id=article.id, field=translated_field.value, action=AddNewLanguageAction.WRITE.value),
+                   user_signature=True,
+                   host=host,
+                   scheme=scheme,
+                   port=port)
+               )
+    
+    if is_textarea:
+        input = TEXTAREA(_id="new-translation", _class="form-control text", _name="new-translation")
+    else:
+        input = INPUT(_id="new-translation", _type='text', _class="string form-control", _name="new-translation")
+    
     return FORM(
         DIV(
             SELECT(lang_options, _id="lang-selector", _class="generic-widget form-control"),
-            BUTTON("Add new translation", _id="add-translation", _class="btn btn-default", _link=url),
+            BUTTON("Write", _id="write-translation", _class="btn btn-default"),
+            BUTTON("Generate", _id="generate-translation", _class="btn btn-success", _link=url_generate, _disabled=True),
+            DIV(
+                input,
+                BUTTON("Save", _id="save-translation", _class="btn btn-primary", _disabled=True, _link=url_write),
+                _id="write-tranlation-block", _style="display: none"
+            ),
             _class="col-sm-9"
         ),
-         _class="form-group"
-    )
+    _class="form-group")
 
 
 def _build_lang_name(lang: LangItem):
@@ -144,23 +215,43 @@ def _build_lang_name(lang: LangItem):
     return lang_name
 
 
-def _generate_lang_form(translation_value: TranslatedFieldDict, is_textarea: bool):
+def _generate_lang_form(article: Article, translated_field: TranslatedFieldType, translation_value: TranslatedFieldDict, is_textarea: bool):
     lang = Lang.get_lang_by_code(translation_value['lang'])
 
     if is_textarea:
         input = TEXTAREA(translation_value["content"], _id=lang.value.code, _class="form-control text", _name=lang.value.code)
     else:
         input = INPUT(_id=lang.value.code, _value=translation_value["content"], _type='text', _class="string form-control", _name=lang.value.code)
+
+    save_url = cast(str,
+                        URL(c="translations",
+                            f="add_or_edit_article_field_translation",
+                            vars=dict(article_id=article.id, field=translated_field.value, action=AddNewLanguageAction.WRITE.value, lang=lang.value.code),
+                            user_signature=True,
+                            host=host,
+                            scheme=scheme,
+                            port=port)
+                    )
+    
+    delete_url = cast(str,
+                        URL(c="translations",
+                            f="delete_translation",
+                            vars=dict(article_id=article.id, field=translated_field.value, lang=lang.value.code),
+                            user_signature=True,
+                            host=host,
+                            scheme=scheme,
+                            port=port)
+                    )
         
     return FORM(
                 _generate_lang_label(lang, translation_value),
                 DIV(
                     input,
-                    A("Save", _class="btn btn-default"),
-                    A("Delete", _class="btn btn-danger"),
+                    A("Save", _class="btn btn-primary lang-form-save-button", _link=save_url),
+                    A("Delete", _class="btn btn-danger lang-form-delete-button", _link=delete_url),
                     _class="col-sm-9"
                 ),
-                _class="form-group", _style="margin-bottom: 10px"
+                _class="form-group", _style="margin-bottom: 10px", _id=f"translation-{lang.value.code}"
             )
 
 
@@ -192,3 +283,8 @@ def _generate_lang_label(lang: Lang, translation_value: TranslatedFieldDict):
         return LABEL(label_text, SPAN('Generated', _style="color: red; margin-left: 5px"), _class=label_class, _for=label_for)
     else:
         return LABEL(label_text, _class=label_class, _for=label_for)
+
+
+class AddNewLanguageAction(Enum):
+    WRITE = 'write'
+    GENERATE = 'generate'
--- static/js/pci/translations.js
+++ static/js/pci/translations.js
@@ -1,21 +1,158 @@
-addTranslationButton = document.getElementById('add-translation');
+const langSelector = document.getElementById('lang-selector');
 
-addTranslationButton.addEventListener('click', addNewLanguage);
+const generateTranslationButton = document.getElementById('generate-translation');
+const writeTranslationButton = document.getElementById('write-translation');
+const saveTranslationButton = document.getElementById('save-translation');
 
-function addNewLanguage(e) {
+generateTranslationButton.addEventListener('click', generateNewTranslation);
+saveTranslationButton.addEventListener('click', saveNewTranslation);
+
+langSelector.addEventListener('change', () => {
+    const disabled = langSelector.value == null || langSelector.value.length === 0;
+
+    generateTranslationButton.disabled = disabled;
+    saveTranslationButton.disabled = disabled;
+
+    const lang = langSelector.value;
+    const forms = document.querySelectorAll('#lang-form-list form');
+    forms.forEach((form) => {
+        if (form.id === `translation-${lang}`) {
+                generateTranslationButton.disabled = true;
+        }
+    });
+});
+
+
+writeTranslationButton.addEventListener('click', (e) => {
     e.preventDefault();
 
-    select = document.getElementById('lang-selector');
-    lang = select.value;
+    const writeTranslationBlock = document.getElementById('write-tranlation-block');
+    if (writeTranslationBlock.style.display === 'none') {
+        writeTranslationBlock.style.display = 'block';
+    } else {
+        writeTranslationBlock.style.display = 'none';
+    }
+});
+
+addListenerLangForm();
+function addListenerLangForm() {
+    const langFormSaveButtons = document.querySelectorAll('.lang-form-save-button');
+    const langFormDeleteButtons = document.querySelectorAll('.lang-form-delete-button');
+
+    langFormSaveButtons.forEach((saveButton) => {
+        saveButton.addEventListener('click', editTranslation);
+    });
     
-    url = new URL(addTranslationButton.getAttribute('link'));
+    langFormDeleteButtons.forEach((deleteButton) => {
+        deleteButton.addEventListener('click', deleteTranslation);
+    });
+}
+
+
+function generateNewTranslation(e) {
+    e.preventDefault();
+    generateTranslationButton.insertAdjacentHTML('afterend', '<span id="generate-status">Generation in progress...</span>');
+
+    const lang = langSelector.value;
+    
+    const url = new URL(generateTranslationButton.getAttribute('link'));
     url.searchParams.append('lang', lang);
 
     $.ajax({
-        type: 'GET',
+        type: 'POST',
         url: url
-    }).done(function(response) {
-        langFormList = document.getElementById('lang-form-list');
+    }).done((response) => {
+        addNewTranslationForm(response, lang);
+    }).always(() => {
+        const el = document.getElementById('generate-status');
+        if (el != null) {
+            el.remove()
+        }
+    });
+}
+
+
+function saveNewTranslation(e) {
+    e.preventDefault();
+
+    const lang = langSelector.value;
+    
+    const url = new URL(saveTranslationButton.getAttribute('link'));
+    url.searchParams.append('lang', lang);
+
+    const translation = document.getElementById('new-translation').value;
+
+    $.ajax({
+        type: 'POST',
+        contentType: 'application/json; charset=utf-8',
+        url: url,
+        data: JSON.stringify({'translation': translation}),
+    }).done((response) => {
+        addNewTranslationForm(response, lang);
+    });
+}
+
+
+function editTranslation(e) {
+    e.preventDefault();
+    
+    const url = new URL(e.target.getAttribute('link'));
+    const lang = url.searchParams.get('lang');
+
+    const translation = document.getElementById(lang).value;
+
+    $.ajax({
+        type: 'POST',
+        contentType: 'application/json; charset=utf-8',
+        url: url,
+        data: JSON.stringify({'translation': translation}),
+    }).done((response) => {
+        addNewTranslationForm(response, lang);
+    });
+}
+
+
+function addNewTranslationForm(response, lang) {
+    if (response === 'None') {
+        return;
+    }
+
+    const langFormList = document.getElementById('lang-form-list');
+
+    const forms = document.querySelectorAll('#lang-form-list form');
+    let remplaced = false;
+    forms.forEach((form) => {
+        if (form.id === `translation-${lang}`) {
+            form.insertAdjacentHTML('beforebegin', response);
+            form.remove();
+            remplaced = true;
+        }
+    });
+    
+    if (!remplaced) {
         langFormList.insertAdjacentHTML('afterbegin', response);
-    })
+    }
+
+    addListenerLangForm();
+}
+
+function deleteTranslation(e) {
+    e.preventDefault();
+    
+    const url = new URL(e.target.getAttribute('link'));
+    const lang = url.searchParams.get('lang');
+
+    $('#confirmation-modal').modal('show')
+		.on('click', '#confirm-dialog', function(){
+			$.ajax({
+                type: 'GET',
+                url: url,
+            }).done(() => {
+                const form = document.getElementById(`translation-${lang}`);
+                form.remove();
+            });
+		});
+
+	$('#cancel-dialog')
+		.on('click',function(){ return; });
 }
