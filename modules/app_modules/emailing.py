5e94a194d059460863c5294b1367e0f23cdbac6e
Charly <charly.maeder@cefe.cnrs.fr>
2024-02-07 16:24:08 +0100

    Fix bug if suggested recommender has been deleted in create_reminder_for_suggested_recommender_invitation.

--- modules/app_modules/emailing.py
+++ modules/app_modules/emailing.py
@@ -2480,14 +2480,15 @@ def create_reminder_for_suggested_recommender_invitation(session, auth, db, arti
     mail_vars = emailing_tools.getMailCommonVars()
 
     article = db.t_articles[articleId]
-    if article:
+    suggested_recommender = User.get_by_id(suggRecommId)
+    if article and suggested_recommender and suggested_recommender.email:
         if pciRRactivated:
             mail_vars.update(getPCiRRScheduledSubmissionsVars(article))
 
         hashtag_template = emailing_tools.getCorrectHashtag("#ReminderSuggestedRecommenderInvitation", article)
 
         mail_vars["destPerson"] = common_small_html.mkUser(auth, db, suggRecommId)
-        mail_vars["destAddress"] = db.auth_user[suggRecommId]["email"]
+        mail_vars["destAddress"] = suggested_recommender.email
 
         mail_vars["articleDoi"] = article.doi
         mail_vars["articleTitle"] = md_to_html(article.title)
