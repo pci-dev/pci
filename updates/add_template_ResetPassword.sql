629c4df502fd0c213e8cb6028b3fe54088020d88
ofa <fambon@github.users>
2024-02-14 17:03:08 +0100

    cleanup updates/*.sql post-release

--- updates/add_template_ResetPassword.sql
+++
@@ -1,9 +0,0 @@
-delete from mail_templates where hashtag = '#UserResetPassword';
-insert into mail_templates
-(hashtag, description, lang, subject, contents) values (
-'#UserResetPassword',
-'Mail to user to reset their password',
-'default',
-'{{appName}}: Reset password',
-'<p>You requested to reset your password. Please click the following link: <a href="{{linkTarget}}">reset password</a>.</p><br><p>Thank you for your time and support.</p>'
-);
