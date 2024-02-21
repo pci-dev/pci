b222e44e9d9273fa7153a1d50cf7e25d4def5561
Charly <charly.maeder@cefe.cnrs.fr>
2024-02-21 17:38:15 +0100

    Add compatibility for biorxiv xml in xml jats parser.

--- modules/app_modules/xml_jats_parser.py
+++ modules/app_modules/xml_jats_parser.py
@@ -2,17 +2,35 @@
 import re
 from typing import List, Optional
 import xml.etree.ElementTree as ET
+import lxml.etree as lxml 
 
 class XMLJATSAuthorElement:
-    first_name: str
-    last_name: str
-    institution: str
-    country: str
-    orcid: str
+    first_name: Optional[str]
+    last_name: Optional[str]
+    institution: Optional[str]
+    country: Optional[str]
+    orcid: Optional[str]
     email: Optional[str]
 
 
     def __init__(self, xml_root: ET.Element, contrib_el: ET.Element):
+        self.first_name = None
+        self.last_name = None
+        self.institution = None
+        self.country = None
+        self.orcid = None
+        self.email = None
+        
+        self._explore_names(contrib_el)
+        self._explore_orcid(contrib_el)
+        self._explore_email(contrib_el)
+        
+        if not self.email:
+            self._explore_corresp(xml_root, contrib_el)
+        self._explore_affiliation(xml_root, contrib_el)
+        
+
+    def _explore_names(self, contrib_el: ET.Element):
         first_name = contrib_el.findtext("./name/given-names")
         if first_name:
             self.first_name = first_name.strip()
@@ -21,17 +39,22 @@ class XMLJATSAuthorElement:
         if last_name:
             self.last_name = last_name.strip()
 
+    
+    def _explore_orcid(self, contrib_el: ET.Element):
         orcid = contrib_el.findtext("./contrib-id")
         if orcid:
             orcid = orcid.replace("http://orcid.org", "")
             orcid = orcid.strip().strip('/').strip()
             self.orcid = orcid
 
-        self._init_affiliation(xml_root, contrib_el)
-        self._init_corresp(xml_root, contrib_el)
+
+    def _explore_email(self, contrib_el: ET.Element):
+        email = contrib_el.findtext("./email")
+        if email:
+            self.email = email.strip()            
 
 
-    def _init_corresp(self, xml_root: ET.Element, contrib_el: ET.Element):
+    def _explore_corresp(self, xml_root: ET.Element, contrib_el: ET.Element):
         corresp_ref = contrib_el.find("./xref[@ref-type='corresp']")
         if corresp_ref == None:
             return
@@ -49,7 +72,7 @@ class XMLJATSAuthorElement:
             self.email = email.strip()
 
 
-    def _init_affiliation(self, xml_root: ET.Element, contrib_el: ET.Element):
+    def _explore_affiliation(self, xml_root: ET.Element, contrib_el: ET.Element):
         affilitation_ref = contrib_el.find("./xref[@ref-type='aff']")
         if affilitation_ref == None:
             return
@@ -60,6 +83,8 @@ class XMLJATSAuthorElement:
         
         affiliation = xml_root.find(f"./front/article-meta/contrib-group/aff[@id='{affiliation_id}']")
         if affiliation == None:
+            affiliation = xml_root.find(f"./front/article-meta/aff[@id='{affiliation_id}']")
+        if affiliation == None:
             return
         
         institution = affiliation.find("./institution")
@@ -67,11 +92,18 @@ class XMLJATSAuthorElement:
             if institution.text:
                 self.institution = institution.text.strip().strip(',').strip()
             if institution.tail:
-                if not self.institution.endswith(' ') and not institution.tail.startswith((' ', ',', '.')):
+                if self.institution and not self.institution.endswith(' ') and not institution.tail.startswith((' ', ',', '.')):
                     self.institution += ' '
-                self.institution += institution.tail.strip().strip(',').strip()
-            self.institution = self.institution.replace("\n", "")
-            self.institution = re.sub(r'\s{2,}', ' ', self.institution)
+                tail = institution.tail.strip().strip(',').strip()
+                if tail:
+                    if not self.institution:
+                        self.institution = tail 
+                    else:
+                        self.institution += tail
+            if self.institution:
+                self.institution = self.institution.replace("\n", "")
+                self.institution = re.sub(r'\s{2,}', ' ', self.institution)
+                self.institution = self.institution.strip()
         
         country = affiliation.findtext("./country")
         if country:
@@ -79,12 +111,17 @@ class XMLJATSAuthorElement:
                     
 
 class XMLJATSArticleElement:
-    title: str
+    title: Optional[str]
     authors: List[XMLJATSAuthorElement]
-    abstract: str
-    doi: str
+    abstract: Optional[str]
+    doi: Optional[str]
 
     def __init__(self, xml_root: ET.Element):
+        self.title = None
+        self.authors = []
+        self.abstract = None
+        self.doi = None
+
         doi = xml_root.findtext("./front/article-meta/article-id[@pub-id-type='doi']")
         if doi:
             self.doi = f"https://doi.org/{doi.strip()}"
@@ -108,7 +145,7 @@ class XMLJATSParser:
     article: XMLJATSArticleElement
 
     def __init__(self, filepath: str):
-        xml_tree = ET.parse(filepath)
+        xml_tree = ET.parse(filepath, lxml.XMLParser(recover=True))
         xml_root = xml_tree.getroot()
 
         self.article = XMLJATSArticleElement(xml_root)
