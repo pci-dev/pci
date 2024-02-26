384f3eb4fca6015b54e95cbab62b882b1878e5b5
ofa <fambon@github.users>
2024-02-26 10:52:36 +0100

    add abstract & keywords tentative ingestion

--- controllers/coar_notify.py
+++ controllers/coar_notify.py
@@ -264,6 +264,16 @@ def map_HAL_json(metadata, content):
     metadata["ms_version"] = c["version_i"]
     metadata["article_year"] = c["publicationDateY_i"]
     metadata["article_source"] = f'c["journalTitle_s"], c["publicationDateY_i"], c["volume_s"], c["pages_s"]'
+    metadata["abstract"] = grab_json_meta(c, "abstract_s", 0)
+    metadata["keywords"] = ", ".join(grab_json_meta(c, "keyword_s"))
+
+
+def grab_json_meta(c, *args):
+    try:
+        for k in args: c = c[k]
+        return c
+    except:
+        return ""
 
 
 def get_link(doi, rel, typ):
