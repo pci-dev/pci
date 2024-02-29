ce02405ce4a586ca9ca193129d9121a54057a4d9
ofa <fambon@github.users>
2024-02-29 10:05:56 +0100

    use direct .delete in original delete-old-article-mails

--- cron_tasks/delete_old_article_mails.py
+++ cron_tasks/delete_old_article_mails.py
@@ -12,10 +12,9 @@ list_mail_to_delete = db(
     & (db.mail_queue.article_id == db.t_articles.id)
     & (db.t_articles.status.belongs(["Cancelled", "Recommended", "Rejected", "Not considered"]))
     & (db.t_articles.last_status_change <= (datetime.now() - timedelta(days=DELETE_DELAY)))
-).select()
+).select(db.mail_queue.id)
 
-for mail in list_mail_to_delete:
-    db(db.mail_queue.id == mail.mail_queue.id).delete()
+db(db.mail_queue.id.belongs(list_mail_to_delete)).delete()
 
 db(
     (db.mail_queue.article_id == None)
