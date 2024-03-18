a98d6662d74ad36d5de3928705ccf40f37edf014
ofa <fambon@github.users>
2024-03-18 15:48:09 +0100

    cleanup updates/*.sql post release

--- updates/add_deleted_to_auth_user.sql
+++
@@ -1,1 +0,0 @@
-alter table auth_user add column deleted boolean default false not null;
--- updates/unsubscription_mail_template.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO mail_templates (hashtag,lang,subject,description,contents) VALUES
-	 ('#UnsubscriptionAlert','default','Unsubscription','Unsubscription','<p><span class="HwtZe" lang="en"><span class="jCAhz ChMk0b"><span class="ryNqvb">User {{person}} [{{address}}] has unsubscribed from {{appLongName}}.</span></span></span></p>');
