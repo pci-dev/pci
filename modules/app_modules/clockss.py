89d33ae504b936b784f5e5556a16a8554737c64a
Charly <charly.maeder@cefe.cnrs.fr>
2024-03-20 13:57:06 +0100

    Use latex to generate clockss pdf file.

--- modules/app_modules/clockss.py
+++ modules/app_modules/clockss.py
@@ -2,7 +2,6 @@ import os
 import ftplib
 import pathlib
 import shutil
-from typing import Literal, Union
 from models.article import Article
 import zipfile as z
 from models.recommendation import Recommendation
@@ -14,60 +13,84 @@ from gluon import current
 
 myconf = AppConfig(reload=True)
 
-scheme = str(myconf.take("alerts.scheme"))
-host = str(myconf.take("alerts.host"))
-port: Union[int, Literal[False]] = myconf.take("alerts.port", cast=lambda v: common_tools.takePort(v))
-server = str(myconf.take("clockss.server"))
-username = str(myconf.take("clockss.username"))
-password = str(myconf.take("clockss.password"))
-
-session = ftplib.FTP(server, username, password) 
-
 
 class ClockssUpload:
 
-    article: Article
+    CLOCKSS_SERVER = str(myconf.take("clockss.server"))
+    CLOCKSS_USERNAME = str(myconf.take("clockss.username"))
+    CLOCKSS_PASSWORD = str(myconf.take("clockss.password"))
+    CLOCKSS_FTP_SESSION = ftplib.FTP(CLOCKSS_SERVER, CLOCKSS_USERNAME, CLOCKSS_PASSWORD) 
+
+    LATEX_TEMPLATE_FILENAME = 'clockss.tex'
+    PDFLATEX_BIN = str(myconf.take("latex.pdflatex"))
+    
+    _article: Article
+    _prefix: str
+    _recommendation: Recommendation
+
     attachments_dir: str
-    prefix: str
-    recommendation: Recommendation
 
     def __init__(self, article: Article):
-        self.article = article
+        self._article = article
         self._init_dir()
-        recommendation = Article.get_last_recommendation(self.article.id)
+        recommendation = Article.get_last_recommendation(self._article.id)
         if not recommendation:
-            raise Exception(f'No recommendation found for article with id: {self.article.id}')
-        self.recommendation = recommendation
+            raise Exception(f'No recommendation found for article with id: {self._article.id}')
+        self._recommendation = recommendation
 
 
     def _build_xml(self):
-        filename = f"{self.attachments_dir}/{self.prefix}.xml"
+        filename = f"{self.attachments_dir}/{self._prefix}.xml"
         crossref.init_conf(current.db)
-        recommendation_xml = crossref.crossref_xml(self.recommendation)
+        recommendation_xml = crossref.crossref_xml(self._recommendation)
         with open(filename, 'wb') as file:
             file.write(recommendation_xml.encode('utf8'))
 
 
     def _init_dir(self):
-        prefix = common_tools.generate_recommendation_doi(self.article.id)[9:]
+        prefix = common_tools.generate_recommendation_doi(self._article.id)[9:]
         attachments_dir = os.path.join(str(current.request.folder), "clockss", prefix)
         os.makedirs(attachments_dir, exist_ok=True)
-        self.prefix = prefix
+        self._prefix = prefix
         self.attachments_dir = attachments_dir
 
 
     def build_pdf(self):
-        options = {
-            'cookie' : self.request.cookies.items()
-        }
-        filename = f"{self.attachments_dir}/{self.prefix}.pdf"
-        printable_page = cast(str, URL(c="articles", f= "rec", vars=dict(articleId=self.article.id, printable=True), host=host, scheme=scheme, port=port))
-        pdfkit.from_url(printable_page, filename, options=options)
-        return f"{self.prefix}.pdf"
+        pdf_path_to_generate = f"{self.attachments_dir}/{self._prefix}.pdf"
+        latex_content = self._build_latex_content_from_template()
+        if latex_content:
+            self._compile_latex(latex_content, pdf_path_to_generate)
+
+        return f"{self._prefix}.pdf"
         
 
-    def _get_latex_template(self):
-        ...
+    def _compile_latex(self, latex_content: str, pdf_dest_path: str):
+        tmp_folder = f"{current.request.folder}/tmp/{self._prefix}"
+        if os.path.exists(tmp_folder):
+            shutil.rmtree(tmp_folder)
+        os.makedirs(tmp_folder)
+
+        latex_file_path = f"{tmp_folder}/{self._prefix}.tex"
+        print(latex_content, file=open(latex_file_path, 'w'))
+
+        os.system(f'{self.PDFLATEX_BIN} --output-directory={tmp_folder} {latex_file_path}')
+        shutil.move(f"{tmp_folder}/{self._prefix}.pdf", pdf_dest_path)
+        shutil.rmtree(tmp_folder)
+
+
+    def _build_latex_content_from_template(self):
+        template = self._get_latex_template_content()
+        if not self._recommendation.recommendation_title:
+            return
+        template = template.replace('[[TEXT]]', self._recommendation.recommendation_title)
+        return template
+
+
+    def _get_latex_template_content(self):
+        template_path = f"{current.request.folder}/templates/{self.LATEX_TEMPLATE_FILENAME}"
+        with open(template_path, 'r') as template:
+            return template.read()
+
 
     def _zip_directory(self, filepath: str):
         direc = pathlib.Path(filepath)
@@ -81,8 +104,8 @@ class ClockssUpload:
         self._zip_directory(self.attachments_dir)
         filename = self.attachments_dir + ".zip"
         with open(filename, 'rb') as file:
-            session.storbinary(f'STOR {self.prefix}.zip', file)
+            self.CLOCKSS_FTP_SESSION.storbinary(f'STOR {self._prefix}.zip', file)
         #delete files after upload
         shutil.rmtree(self.attachments_dir)
         os.remove(filename)
-        session.quit()
+        self.CLOCKSS_FTP_SESSION.quit()
