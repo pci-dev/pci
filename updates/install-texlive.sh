e52c8d1f951f5c8e7cddd2a76bcd28901d88a7b8
Charly <charly.maeder@cefe.cnrs.fr>
2024-04-03 11:31:49 +0200

    Add install latex script.

---
+++ updates/install-texlive.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Example : ./install-texlive.sh EB3
+
+APP=$1
+if [ -z $APP ]; then
+    echo "APP name missing"
+    exit
+fi
+
+if [ ! -d "$HOME/texlive" ]; then
+    cd /tmp
+    curl -L -o install-tl-unx.tar.gz https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
+    zcat < install-tl-unx.tar.gz | tar xf -
+    cd install-tl-20240401
+    perl ./install-tl --no-interaction --texdir=$HOME/texlive
+else
+    echo "$HOME/texlive already exists"
+fi
+
+cd $HOME
+
+PDFLATEX_BIN=$PWD/texlive/bin/x86_64-linux/pdflatex
+CONFIG_FILE=$PWD/sites/$APP/private/appconfig.ini
+
+echo -e "\n[latex]\npdflatex = $PDFLATEX_BIN" >> $CONFIG_FILE
