36941decf253de81ead70940d0d4e6f5d859990c
ofa <fambon@github.users>
2024-04-26 16:01:58 +0200

    exit 1 if $HOME/texlive exists

--- utils/install-texlive.sh
+++ utils/install-texlive.sh
@@ -8,15 +8,16 @@ if [ -z $APP ]; then
     exit
 fi
 
-if [ ! -d "$HOME/texlive" ]; then
+if [ -d "$HOME/texlive" ]; then
+    echo "$HOME/texlive already exists"
+    exit 1
+fi
+
     cd /tmp
     curl -L -o install-tl-unx.tar.gz https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
     zcat < install-tl-unx.tar.gz | tar xf -
     cd `ls | grep install-tl-2 | tail -1`
     perl ./install-tl --no-interaction --texdir=$HOME/texlive
-else
-    echo "$HOME/texlive already exists"
-fi
 
 cd $HOME
 
