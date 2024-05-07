be69bf25141c6b5057397917269069942f766464
ofa <fambon@github.users>
2024-05-07 10:50:56 +0200

    add 'funding' in edit_my_article (parallel subs)

--- controllers/user.py
+++ controllers/user.py
@@ -772,6 +772,7 @@ def edit_my_article():
             "scripts_doi", 
             "codes_used_in_study", 
             "codes_doi", 
+            "funding",
         ]
 
         fields += ["thematics"]
