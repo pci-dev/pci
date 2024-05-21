cc9f2454eefb30efc7ac79eab941c5c9c20a4c83
Charly <charly.maeder@cefe.cnrs.fr>
2024-05-21 17:40:30 +0200

    Add feature to saved article form for new article submission (with session for moment).

--- static/js/pci/article_form_common.js
+++ static/js/pci/article_form_common.js
@@ -29,19 +29,19 @@ function getMimeType(base64Data) {
 }
   
 
-function initUploadedPictureField() {
+function initUploadedPictureField(storageKey, storage) {
     const uploadFileInput = document.getElementById('t_articles_uploaded_picture');
     if (!uploadFileInput || uploadFileInput.files.length > 0) {
         return;
     }
 
-    const item = JSON.parse(sessionStorage.getItem('t_articles_uploaded_picture'));
+    const item = JSON.parse(storage.getItem(storageKey));
     if (item == null) {
         return;
     }
 
     const base64Data = item.base64;
-    const mimeType = getMimeType(base64Data)
+    const mimeType = getMimeType(base64Data);
     const blob = dataURItoBlob(base64Data, mimeType);
     const file = new File([blob], item.filename, {type: mimeType, lastModified: new Date().getTime() });
 
@@ -51,18 +51,77 @@ function initUploadedPictureField() {
 }
 
 
+async function saveUploadedPictureInStorage(fileInput, storageKey, storage) {
+    const base64Data = await getBase64(fileInput);
+    storage.setItem(storageKey, JSON.stringify({
+        pathName: window.location.pathname,
+        filename: fileInput.name,
+        base64: base64Data}));
+}
+
+////
+
 document.getElementById('t_articles_uploaded_picture')?.addEventListener('change', async(e) => {
     if (e.target.files.length === 0) {
         return;
     }
-
-    const file = e.target.files[0];
-    const base64Data = await getBase64(file);
-    sessionStorage.setItem('t_articles_uploaded_picture', JSON.stringify({
-        pathName: window.location.pathname,
-        filename: file.name,
-        base64: base64Data}));
+    saveUploadedPictureInStorage(e.target.files[0], 't_articles_uploaded_picture', sessionStorage);
 });
 
 
-initUploadedPictureField();
+initUploadedPictureField('t_articles_uploaded_picture', sessionStorage);
+
+////
+
+document.getElementById('save-article-form-button').addEventListener('click', saveForm);
+document.getElementById('clean-save-article-form-button').addEventListener('click', cleanFormSaved);
+
+function getSaveFormUploadedPictureLabel() {
+    return `save-form-${window.location.pathname}-t_articles_uploaded_picture`;
+}
+
+
+async function saveForm(e) {
+    const values = {
+        data_doi: []
+    };
+
+    const data_doi_ul = document.getElementById('t_articles_data_doi_grow_input');
+    const data_doi_inputs = data_doi_ul.getElementsByTagName('input');
+    for (const input of data_doi_inputs) {
+      values.data_doi.push(input.value)
+    }
+
+    localStorage.setItem(`save-form-${window.location.pathname}`, JSON.stringify(values));
+
+    const uploadedPicture = document.getElementById('t_articles_uploaded_picture')?.files[0];
+    saveUploadedPictureInStorage(uploadedPicture, getSaveFormUploadedPictureLabel(), localStorage);
+}
+
+
+function loadFormSaved() {
+    const values = JSON.parse(localStorage.getItem(`save-form-${window.location.pathname}`));
+    if (values == null) {
+        return;
+    }
+
+    const data_doi_ul = document.getElementById('t_articles_data_doi_grow_input');
+    if (values.data_doi.length > 0) {
+        data_doi_ul.innerHTML = ''
+    }
+
+    for (const value of values.data_doi) {
+        data_doi_ul.innerHTML += `
+        <li>
+            <div class="input-group" style="width: 100%">
+                <input class="string form-control" id="t_articles_data_doi" name="data_doi" type="text" value="${value}">
+            </div>
+        </li>`;
+    }
+
+    const uploadedPicture = document.getElementById('t_articles_uploaded_picture');
+    initUploadedPictureField(getSaveFormUploadedPictureLabel(), localStorage);
+
+    document.getElementById('clean-save-article-form-button').style.display = 'inline-block';
+}
+loadFormSaved();
