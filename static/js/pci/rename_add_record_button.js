0582361770a47ee70d8c63bf2e2b42989e061cf0
Charly <charly.maeder@cefe.cnrs.fr>
2024-07-03 15:59:25 +0200

    Fix type error in js file.

--- static/js/pci/rename_add_record_button.js
+++ static/js/pci/rename_add_record_button.js
@@ -1,10 +1,11 @@
 $(function() {
     $('span')
     .filter(function(i) {
-        return (
-	    $(this).attr("title") | ""
-        )
-        .indexOf("Add record to database") != -1
+        const title =  ($(this).attr("title") | "")
+
+        if (typeof title === 'string') {
+            return title.indexOf("Add record to database") != -1;
+        }
     })
     .each(function(i) {
 	$(this)
