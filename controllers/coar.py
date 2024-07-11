2c0b56bd76712d6823622a6639457d4d182e5727
ofa <fambon@github.users>
2024-07-11 11:11:11 +0200

    update text on page coar/complete_submission

--- controllers/coar.py
+++ controllers/coar.py
@@ -78,16 +78,16 @@ Confirm submission cancellation
 """
 
 _page_text = """
-<h2>Please read the following information attentively</h2>
+<h1>Please read the following information carefully</h1>
 <p>
-You have just submitted your preprint to PCI via an open archive. <b>Before completing the submission process, please carefully check the following points</b>:
+You are in the process of submitting your preprint to PCI via an open archive. <b>Before completing your submission, please read and take note of the following</b>:
 </p>
 <ul>
 <li>{appName} is a preprint reviewing service and <b>not a journal</b>,</li>
-<li>If <b>positively evaluated</b> by {appName}, your preprint will be <b>publicly recommended</b> and you could then <b>transfer it for publication in the Peer Community Journal or submit it to a PCI-friendly journal</b>,</li>
-<li>If your preprint contains <b>data, scripts</b> (e.g. for statistical analysis, like R scripts) <b>and/or codes</b> (e.g. codes for original programs or software), they <b>should be made available to reviewers</b> at submission. Data, scripts or codes must be carefully described such that another researcher can reuse them. If your preprint is eventually recommended, data and script should be made available to the readers either in the text or through a correctly versioned deposit in an open repository with a DOI or another permanent identifier (such as a SWHID of <a href="https://www.softwareheritage.org/" target="_blank">Software heritage</a>).</li>
+<li>If <b>your preprint receives a positive evaluation</b> from {appName}, it will be <b>publicly recommended</b> and you can then <b>request its publication in the <a href="https://peercommunityjournal.org/">Peer Community Journal</a> or submit it to a <a href="https://peercommunityin.org/pci-friendly-journals/">PCI-friendly journal</a></b>, or to any journal of your choice accepting the submissions of preprint.</li>
+<li>If your preprint contains <b>data, scripts</b> (e.g. for statistical analysis, such as R scripts) <b>and/or codes</b> (e.g. code for original programs or software), they <b>should be made available to reviewers</b> at submission. Data, scripts or code must be carefully described such that another researcher can reuse them. If your preprint is eventually recommended, data and scripts should be made available to the readers either in the text or through a correctly versioned deposition in an open repository with a DOI or another permanent identifier (such as a SWHID of <a href="https://www.softwareheritage.org/" target="_blank">Software heritage</a>). Note that Git URLs are not permanent. Information on how to issue a DOI for a GitHub repository is given at <a href="https://docs.github.com/en/repositories/archiving-a-github-repository/referencing-and-citing-content">Referencing and citing content - GitHub Docs</a>. Data, scripts or code must be carefully described such that another researcher can reuse them.</li>
 <li>Your <b>preprint must not be published or under consideration for evaluation elsewhere</b> at the time of its submission to {appName}. If your preprint is sent out for review by {appName}, you are not permitted to submit it to a journal until the {appName} evaluation process has been completed. You cannot, therefore, submit your preprint to a journal before its rejection or recommendation by {appName},</li>
-<li><b>You and your co-authors should have no financial conflict of interest</b> (see a definition <a href="{aboutEthicsLink}">here</a>) relating to the articles you submit. If you are unsure whether your article may be associated with financial conflicts of interest, please send an Email to contact@peercommunityin.org to ask for clarification.</li>
+<li><b>You and your co-authors should have no financial conflict of interest</b> (see a definition <a href="{aboutEthicsLink}">here</a>) relating to the articles you submit. If you are unsure about whether there are financial conflicts of interest associated with your article, please send an e-mmail to contact@peercommunityin.org to request clarification.</li>
 </ul>
 <p>
 <b>Please note that</b>:
@@ -95,11 +95,13 @@ You have just submitted your preprint to PCI via an open archive. <b>Before comp
 <ul>
 <li>It can take up to 20 days before a recommender decides to handle your preprint and therefore to send it out for peer-review.</li>
 <li>The median time between submission and the recommender's decision based on the first round of peer-review is currently 50 days.</li>
-<li>The evaluation of your preprint might take several rounds of review before the recommender take the final decision to reject or recommend your preprint. </li>
+<li>The evaluation of your preprint may involve several rounds of review before the recommender takes the final decision to reject or recommend your preprint. </li>
+<li>PCI will send private notifications of decisions (revise, reject and accept) and reviews to the open archive from which you initiated submission to PCI. PCI will also send you e-mail messages to notify you of these decisions.</li>
+<li>Only the acceptance decision and the associated recommendation text, peer reviews and authors’ responses will be made public on the PCI and open archive websites.</li>
 <li>Details about the evaluation & recommendation process can be found <a href="{helpGenericLink}">here</a>.</li>
 </ul>
 <p>
-By clicking on the green button below, you confirm that you agree to comply with the <a href="{gtuLink}">General Terms of Use</a> and the <a href="{aboutEthicsLink}">code of conduct</a>.
+By clicking on the green button below, you confirm that you agree with the above information and will comply with the <a href="{gtuLink}">General Terms of Use</a> and the <a href="{aboutEthicsLink}">code of conduct</a>.
 </p>
 <hr style="border-top: 1px solid #dddddd; margin: 15px 0px;">
 <div style="width: 100%; text-align: center; margin-bottom: 25px;"><a href="{completeSubmissionLink}" style="text-decoration: none;"><span style="margin: 10px; font-size: 14px; font-weight: bold; color: white; padding: 5px 15px; border-radius: 5px; hyphens: none;background: #93c54b">Complete your submission</span></a><b></b><a href="{cancelSubmissionLink}" style="text-decoration: none;"><span style="margin: 10px; font-size: 14px; font-weight:bold; color: white; padding: 5px 15px; border-radius: 5px; hyphens: none;background: #f47c3c">Cancel your submission</span></a></div>
