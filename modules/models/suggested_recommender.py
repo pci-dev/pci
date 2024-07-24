7af6ddc4a872c62108f64da21a833d362dee2520
Charly <charly.maeder@cefe.cnrs.fr>
2024-07-24 16:43:30 +0200

    Add new model function to accept/decline/search suggested recommender.

--- modules/models/suggested_recommender.py
+++ modules/models/suggested_recommender.py
@@ -1,3 +1,4 @@
+import secrets
 from typing import List, Optional as _, cast
 from pydal.objects import Row
 from gluon import current
@@ -17,7 +18,14 @@ class SuggestedRecommender(Row):
     def get_by_id(id: int):
         db = current.db
         return cast(_[SuggestedRecommender], db.t_suggested_recommenders[id])
-    
+
+
+    @staticmethod
+    def get_by_article_and_user_id(article_id: int, user_id: int):
+        db = current.db
+        suggested_recommender: _[SuggestedRecommender] = db((db.t_suggested_recommenders.article_id == article_id) & (db.t_suggested_recommenders.suggested_recommender_id == user_id)).select().first()
+        return suggested_recommender
+
 
     @staticmethod
     def get_suggested_recommender_by_article(article_id: int):
@@ -32,4 +40,27 @@ class SuggestedRecommender(Row):
             return int(db((db.t_suggested_recommenders.article_id == article_id) & (db.t_suggested_recommenders.declined == True)).count())
         else:
             return int(db(db.t_suggested_recommenders.article_id == article_id).count())
-    
+
+
+    @staticmethod
+    def get_suggested_recommender_by_quick_decline_key(quick_decline_key: str):
+        db = current.db
+        suggested_recommender: _[SuggestedRecommender] = db(db.t_suggested_recommenders.quick_decline_key == quick_decline_key).select().first()
+        return suggested_recommender
+
+
+    @staticmethod
+    def decline(suggested_recommender: 'SuggestedRecommender'):
+        suggested_recommender.declined = True
+        suggested_recommender.update_record()
+        current.db.commit()
+
+
+    @staticmethod
+    def add_suggested_recommender(recommender_id: int, article_id: int):
+        db = current.db
+        quick_decline_key = secrets.token_urlsafe(64)
+        db.t_suggested_recommenders.update_or_insert(
+            suggested_recommender_id=recommender_id,
+            article_id=article_id,
+            quick_decline_key=quick_decline_key)
