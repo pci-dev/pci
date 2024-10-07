342fb6ab3a257789ee16dd0239da9f31b553deb0
Charly <charly.maeder@cefe.cnrs.fr>
2024-10-07 17:52:01 +0200

    Check presence of more pending mail for recommender in timeline

--- modules/models/mail_queue.py
+++ modules/models/mail_queue.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from enum import Enum
 import re
-from typing import List, Optional as _, cast
+from typing import Any, List, Optional as _, Union, cast
 from models.article import Article
 from models.review import Review
 from pydal.objects import Row
@@ -66,22 +66,35 @@ class MailQueue(Row):
 
 
     @staticmethod
-    def get_by_article_and_template(article: Article, hastag_template: str, sending_status: List[SendingStatus] = []):
+    def get_by_article_and_template(article: Article,
+                                    hastag_template: Union[str, List[str]],
+                                    sending_status: List[SendingStatus] = [],
+                                    order_by: _[Any] = None) -> List['MailQueue']:
         db = current.db
         sending_status_values = [s.value for s in sending_status]
 
+        if isinstance(hastag_template, str):
+            hastag_template = [hastag_template]
+
+
         if len(sending_status_values) == 0:
-            mails = db(
+            query = db(
                 (db.mail_queue.article_id == article.id) &
-                (db.mail_queue.mail_template_hashtag == hastag_template)
-            ).select()
+                (db.mail_queue.mail_template_hashtag.belongs(hastag_template))
+            )
         else:
-            mails = db(
+            query = db(
                 (db.mail_queue.article_id == article.id) &
-                (db.mail_queue.mail_template_hashtag == hastag_template) &
+                (db.mail_queue.mail_template_hashtag.belongs(hastag_template)) &
                 (db.mail_queue.sending_status.belongs(sending_status_values))
-            ).select()
-        return cast(List[MailQueue], mails)
+            )
+
+        if order_by:
+            mails = query.select(orderby=order_by)
+        else:
+            mails = query.select()
+            
+        return mails
 
 
     @staticmethod
