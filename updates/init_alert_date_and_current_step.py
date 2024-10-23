8b86cef2ae830413c3eb4610dde3899293369443
ofa <fambon@github.users>
2024-10-23 10:19:47 +0200

    remove one-off updates/ scripts

--- updates/init_alert_date_and_current_step.py
+++
@@ -1,9 +0,0 @@
-from models.article import Article
-
-if __name__ == '__main__':
-
-    articles = Article.get_all()
-
-    for article in articles:
-        Article.update_alert_date(article)
-        Article.update_current_step(article)
