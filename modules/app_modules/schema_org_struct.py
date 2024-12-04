7b18784716fd4b3a092296534a32569faf3b3305
Charly <charly.maeder@cefe.cnrs.fr>
2024-12-04 12:01:32 +0100

    Add specific element for google search engine

--- modules/app_modules/schema_org_struct.py
+++ modules/app_modules/schema_org_struct.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import datetime
 from typing import List, Union
 
 from serde import Optional, Untagged, dataclass, serde, field
@@ -12,14 +12,20 @@ class TranslatedField:
 
 
 @serde(rename_all="camelcase")
+class Id:
+    id: str = field(rename="@id")
+
+
+@serde(rename_all="camelcase")
 class CreativeWork:
-    type: str = field(init=False, rename="@type", default="schema:CreativeWork")
+    type: str = field(init=False, rename="@type", default="CreativeWork")
     headline: str
+    name: str
 
 
 @serde(rename_all="camelcase")
 class Person:
-    type: str = field(init=False, rename="@type", default="schema:Person")
+    type: str = field(init=False, rename="@type", default="Person")
     name: str
     identifier: Optional[str] = field(skip_if_false=True, default=None)
     url: Optional[str] = field(skip_if_false=True, default=None)
@@ -27,75 +33,86 @@ class Person:
 
 @serde(rename_all="camelcase")
 class MediaObject:
-    type: str = field(init=False, rename="@type", default="schema:MediaObject")
+    type: str = field(init=False, rename="@type", default="MediaObject")
+    name: str
     content_url: Optional[str] = field(skip_if_false=True, default=None)
-    name: Optional[str] = field(skip_if_false=True, default=None)
     archived_at: Optional[str] = field(skip_if_false=True, default=None)
     description: Optional[str] = field(skip_if_false=True, default=None)
 
 
 @serde(rename_all="camelcase")
 class Answer:
-    type: str = field(init=False, rename="@type", default="schema:Answer")
+    type: str = field(init=False, rename="@type", default="Answer")
+    name: str
     author: List[Person]
     text: Optional[str] = field(skip_if_false=True, default=None)
     content_url: Optional[str] = field(skip_if_false=True, default=None)
-    name: Optional[str] = field(skip_if_false=True, default=None)
-    associated_media: Optional[MediaObject] = field(skip_if_false=True, default=None)
-
-
-@serde(rename_all="camelcase")
-class CriticReview:
-    type: str = field(init=False, rename="@type", default="schema:CriticReview")
-    author: Person
-    date_published: date
-    review_body: Optional[str] = field(skip_if_false=True, default=None)
     associated_media: Optional[MediaObject] = field(skip_if_false=True, default=None)
 
 
 @serde(rename_all="camelcase")
 class Periodical:
-    type: str = field(init=False, rename="@type", default="schema:Periodical")
+    type: str = field(init=False, rename="@type", default="Periodical")
     name: str
+
+
+@serde(rename_all="camelcase")
+class Comment:
+    type: str = field(init=False, rename="@type", default="Comment")
+    name: str
+    author: Person
+    parent_item: Id
+    date_published: datetime
+    text: Optional[str] = field(skip_if_false=True, default=None)
+    shared_content: Optional[MediaObject] = field(skip_if_false=True, default=None)
 
 
 @dataclass
 class ScholarlyArticle:
+    id: str
     same_as: str
-
-    type: str = field(init=False, rename="@type", default="schema:ScholarlyArticle")
+    name: str
+    type: str = field(init=False, rename="@type", default="ScholarlyArticle")
 
     headline: Optional[Union[List[TranslatedField], str]] = field(skip_if_false=True, default=None)
     author: Optional[List[Person]] = field(skip_if_false=True, default=None)
-    date_created: Optional[date] = field(skip_if_false=True, default=None)
-    date_published: Optional[date] = field(skip_if_false=True, default=None)
+    date_created: Optional[datetime] = field(skip_if_false=True, default=None)
+    date_published: Optional[datetime] = field(skip_if_false=True, default=None)
     identifier: Optional[str] = field(skip_if_false=True, default=None)
     abstract: Optional[Union[List[TranslatedField], str]] = field(skip_if_false=True, default=None)
+    article_body: Optional[Union[List[TranslatedField], str]] = field(skip_if_false=True, default=None)
     is_part_of: Optional[Periodical] = field(skip_if_false=True, default=None)
     citation: Optional[List[Union["ScholarlyArticle", CreativeWork]]] = field(skip_if_false=True, default=None)
     about: Optional[str] = field(skip_if_false=True, default=None)
-    is_based_on: Optional["ScholarlyArticle"] = field(skip_if_false=True, default=None)
+    is_based_on: Optional[List[Union["ScholarlyArticle", Comment, "Recommendation"]]] = field(
+        skip_if_false=True, default=None
+    )
     image: Optional[str] = field(skip_if_false=True, default=None)
     archived_at: Optional[str] = field(skip_if_false=True, default=None)
     version: Optional[str] = field(skip_if_false=True, default=None)
     associated_media: Optional[List[MediaObject]] = field(skip_if_false=True, default=None)
     keywords: Optional[Union[List[TranslatedField], str]] = field(skip_if_default=True, default=None)
     funder: Optional[str] = field(skip_if_default=True, default=None)
-    associated_review: Optional[List[Union[CriticReview, "Recommendation"]]] = field(skip_if_default=True, default=None)
+    review: Optional["Recommendation"] = field(skip_if_default=True, default=None)
+    comment: Optional[List[Comment]] = field(skip_if_default=True, default=None)
 
 
 @dataclass
 class Recommendation:
-    date_created: date
-    date_published: date
-    review_body: str
+    date_created: datetime
+    date_published: datetime
+    article_body: str
     author: Person
     comment: Answer
-    item_reviewed: ScholarlyArticle
+    name: str
+    headline: str
 
-    type: str = field(init=False, rename="@type", default="schema:Recommendation")
+    type: str = field(init=False, rename="@type", default="ScholarlyArticle")
     associated_media: Optional[List[MediaObject]] = field(skip_if_false=True, default=None)
-    associated_review: Optional[List[Union[CriticReview, "Recommendation"]]] = field(skip_if_false=True, default=None)
+    associated_review: Optional["Recommendation"] = field(skip_if_false=True, default=None)
+    is_based_on: Optional[List[Union[ScholarlyArticle, Comment, "Recommendation"]]] = field(
+        skip_if_false=True, default=None
+    )
 
 
 serde(Recommendation, rename_all="camelcase", tagging=Untagged)
@@ -103,15 +120,15 @@ serde(ScholarlyArticle, rename_all="camelcase", tagging=Untagged)
 
 
 @serde(rename_all="camelcase")
-class Context:
-    schema: str = field(init=False, default="https://schema.org/")
-    language: str = field(init=False, rename="@language", default="en")
+class LanguageContainer:
+    id: str = field(rename="@id")
+    container: str = field(init=False, rename="@container", default="@language")
 
 
 @serde(rename_all="camelcase")
 class Root:
     graph: ScholarlyArticle = field(rename="@graph")
-    context: Context = field(init=False, rename="@context", default=Context())
+    context: str = field(init=False, rename="@context", default="https://schema.org/")
 
     def to_json(self):
         return serde_to_json(self)
