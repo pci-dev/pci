307a49c44bc731fa75750487d5e840c46adc49c4
ofa <fambon@github.users>
2025-02-12 10:58:28 +0100

    move articles/metadata to metadata/recommendation (new controller)

--- controllers/articles.py
+++ controllers/articles.py
@@ -124,81 +124,12 @@ def handle_rec_signposting(recomm):
     if request.env.request_method == 'HEAD':
 
         response.headers = { "link": (
-            '<' + URL("articles", "metadata", scheme=True,
-                vars=dict(recommId=recomm.id)
+            '<' + URL("metadata", "recommendation", scheme=True,
+                vars=dict(id=recomm.id)
             ) + '>' +
             '; rel="describedby" type="docmaps"'
         )}
         return True
-
-
-def metadata():
-    recommId = request.vars.recommId
-    response.headers.update({
-        "Content-type": "text/json",
-    })
-    import json
-    return json.dumps([
-  {
-    "type": "docmap",
-    "id": URL('articles', 'rec', scheme=True, vars=dict(recommId=recommId)),
-    "publisher": {
-      "name": "Inferred from Crossref",
-      "url": "https://github.com/docmaps-project/docmaps/"
-    },
-    "created": "2025-02-11T23:08:47.393Z",
-    "updated": "2025-02-11T23:08:47.393Z",
-    "first-step": "_:b0",
-    "steps": {
-      "_:b0": {
-        "inputs": [],
-        "actions": [
-          {
-            "participants": [
-              {
-                "actor": {
-                  "type": "person",
-                  "name": "Perrodin, Catherine"
-                },
-                "role": "author"
-              },
-              {
-                "actor": {
-                  "type": "person",
-                  "name": "Verzat, Colombine"
-                },
-                "role": "author"
-              },
-              {
-                "actor": {
-                  "type": "person",
-                  "name": "Bendor, Daniel"
-                },
-                "role": "author"
-              }
-            ],
-            "outputs": [
-              {
-                "published": "2020-01-28T00:00:00.000Z",
-                "doi": "10.1101/2020.01.28.922773",
-                "type": "preprint"
-              }
-            ],
-            "inputs": []
-          }
-        ],
-        "assertions": [
-          {
-            "status": "catalogued",
-            "item": "10.1101/2020.01.28.922773"
-          }
-        ]
-      }
-    },
-    "@context": "https://w3id.org/docmaps/context.jsonld"
-  }
-]
-)
 
 
 ######################################################################################################################################################################
