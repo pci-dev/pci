e054c1cc4415acf2e4807394ad567e83e3259928
Charly <charly.maeder@cefe.cnrs.fr>
2025-02-19 17:41:37 +0100

    Add feature in fetch_doi script to search article doi for all article updated yesterday in crossref

--- cron_tasks/fetch_doi.py
+++ cron_tasks/fetch_doi.py
@@ -1,4 +1,7 @@
+from datetime import datetime, timedelta
 from time import sleep
+from typing import List
+from app_modules.crossref_api import CrossrefAPI
 from gluon import current
 from models.article import Article
 from app_modules import emailing
@@ -8,6 +11,52 @@ def main():
     articles = Article.get_all_articles_without_article_published_doi()
     print(f"Number of article without published article DOI: {len(articles)}")
 
+    count = process_request_all_api(articles)    
+    print(f"Fetch DOI from API finished: {count} published article DOI added")
+
+    count = search_in_crossref(articles)
+    print(f"Search in yesterday updated work in Crossref finished: {count} published article DOI added")
+
+
+def search_in_crossref(articles: List[Article]):
+    count = 0
+
+    yesterday = (datetime.now() - timedelta(1)).date()
+
+    preprint_dois: List[str] = []
+    for article in articles:
+        if article.doi:
+            preprint_dois.append(article.doi)
+
+    try:
+        article_dois = CrossrefAPI().get_published_article_doi_since(preprint_dois, yesterday)
+    except Exception as e:
+        print(f"Error to search in Crossref API since {yesterday}: {e}")
+        return
+    
+    if len(article_dois) == 0:
+        return 0
+
+    for article in articles:
+        if not article.doi:
+            continue
+
+        preprint_doi = article.doi.strip()
+        article_doi = article_dois.get(preprint_doi)
+
+        if article_doi:
+            current.db(current.db.t_articles.id == article.id).update(doi_of_published_article=article_doi)
+            current.db.commit()
+            count += 1
+            if 'pcjournal' in article_doi:
+                emailing.send_message_to_recommender_and_reviewers(article.id)
+
+            print(f"Published article DOI found for article {article.id}: {article_doi}")
+
+    return count
+
+
+def process_request_all_api(articles: List[Article]):
     count = 0
 
     for article in articles:
@@ -29,9 +78,7 @@ def main():
         finally:
             sleep(0.1)
     
-    print(f"Fetch DOI finished: {count} published article DOI added")
-    
-
+    return count
 
 if __name__ == "__main__":
     main()
--- modules/app_modules/crossref_api.py
+++ modules/app_modules/crossref_api.py
@@ -1,6 +1,7 @@
+from datetime import date
+from time import sleep
 from app_modules.httpClient import HttpClient
-from typing import Any, List, Optional
-
+from typing import Any, Dict, List, Optional
 
 class CrossrefAPI:
 
@@ -41,3 +42,71 @@ class CrossrefAPI:
             doi = str(relation['id'])
             if doi:
                 return doi_to_url(doi)
+
+
+    def get_published_article_doi_since(self, preprint_doi_url: List[str], start_date: date):
+        ''' Return dict where keys are preprint doi url and values journal doi url'''
+
+        from app_modules.common_tools import url_to_doi_id, doi_to_url
+
+        preprint_dois = [url_to_doi_id(d) for d in preprint_doi_url]
+        works = self._get_works_article_journal_newer_than(start_date)
+
+        journal_dois: Dict[str, str] = {}
+
+        for preprint_doi in preprint_dois:
+            for work in works:
+                journal_doi: Optional[str] = work.get('DOI')
+                if not journal_doi:
+                    continue
+                
+                references: Optional[List[Any]] = work.get('reference')
+                if not references:
+                    continue
+
+                for reference in references:
+                    reference_doi: Optional[str] = reference.get('DOI')
+                    if not reference_doi:
+                        continue
+                    
+                    if preprint_doi in reference_doi:
+                        journal_dois[doi_to_url(preprint_doi)] = doi_to_url(journal_doi)
+                    
+        return journal_dois
+
+
+    def _get_works_article_journal_newer_than(self, start_date: date):
+        from app_modules.common_tools import sget
+
+        pub_date = start_date.isoformat()
+
+        cursor_size = 1000
+
+        cursor = "*"
+        url = f"{self.BASE_URL}/works?rows={cursor_size}&select=DOI,reference,type&filter=type:journal-article,from-update-date:{pub_date},relation.type:has-preprint"
+
+        works: List[Any] = []
+
+        while True:
+            response = self._http_client.get(f"{url}&cursor={cursor}")
+            if not response.ok:
+                break
+
+            data = response.json() # type: ignore
+            items: Optional[List[Any]] = sget(data, 'message', 'items')
+            if not items:
+                break
+
+            works.extend(items)
+
+            if len(items) != cursor_size:
+                break
+            
+            cursor = sget(data, 'message', 'next-cursor')
+            if not cursor:
+                break
+
+            sleep(0.1)
+        
+        return works
+    
