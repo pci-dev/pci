6d6dd9aa7f2e061240da9dbf92a8b8a4ad986607
Charly <charly.maeder@cefe.cnrs.fr>
2025-03-06 16:02:56 +0100

    Add step text to valid sugg_reco and fix reviewer page

--- tests/test_setup_article.py
+++ tests/test_setup_article.py
@@ -194,6 +194,13 @@ class Manager_validates:
     select.fails("tr", contains=article.title)
     
 
+ def validate_sugg_recommender(_):
+    select(".dropdown-toggle", contains="For managers").click()
+    select("a", f"Manage suggested recommenders").click()
+
+    select("a", "VALID").click()
+    select.notif('Suggested recommender "recommender dude" has been validated').wait_clickable()
+
  def logout_manager(_):
     logout(users.manager)
 
