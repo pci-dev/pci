b6a44cf98c1a14773a4b692079bb8f9defe526d8
Charly <charly.maeder@cefe.cnrs.fr>
2025-03-24 12:03:50 +0100

    Add template mail #WillingRecommenderValidation

--- sql_dumps/insert_common_mail_templates.sql
+++ sql_dumps/insert_common_mail_templates.sql
@@ -156,3 +156,17 @@ INSERT INTO public.mail_templates (hashtag,lang,subject,description,contents) VA
 <div><br>As a reminder, {{submitterPerson}} has suggested one or more other recommenders to handle their submitted preprint entitled {{articleTitle}}.</div>
 <div><br>Please follow this link {{linkTarget}} or click the buttons below to validate or not these suggested recommenders.</div>
 <div><br>Thanks in advance.<br>Have a nice day!</div>');
+
+--- 2025-03-10 updates/willing_reco_validation.sql
+
+INSERT INTO public.mail_templates (hashtag,lang,subject,description,contents) VALUES
+	 ('#WillingRecommenderValidation','default','{{appName}}: New willing recommender pending validation','New willing recommender pending validation','<div>Dear members of the Managing Board,</div>
+<div>&nbsp;</div>
+<div><span class="HwtZe" lang="en"><span class="jCAhz ChMk0b"><span class="ryNqvb">Recommender <strong>{{recommenderPerson}}</strong> is willing to become the recommender in charge of the preprint entitled <strong>{{articleTitle}}</strong> - via the "<strong>Preprints in need of a recommender</strong>" page.</span></span></span></div>
+<div>&nbsp;</div>
+<div>If you want them to handle the preprint, you must accept the suggestion so that they will receive an invitation email.</div>
+<div>&nbsp;</div>
+<div><span class="HwtZe" lang="en"><span class="jCAhz ChMk0b">Please follow this link <a href="{{linkTarget}}">{{linkTarget}}</a> to accept or decline this suggestion.<br></span></span></div>
+<div>&nbsp;</div>
+<div><span class="HwtZe" lang="en"><span class="jCAhz ChMk0b">Thanks in advance.</span></span></div>
+<div><span class="HwtZe" lang="en"><span class="jCAhz ChMk0b"><br>Have a nice day!</span></span></div>');
