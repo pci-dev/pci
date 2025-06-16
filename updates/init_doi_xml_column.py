5037355ed2b878ce648c6538fe9ae8f660bd2d02
Charly Maeder <charly.maeder@cefe.cnrs.fr>
2025-06-16 17:56:13 +0200

    save

--- updates/init_doi_xml_column.py
+++ updates/init_doi_xml_column.py
@@ -4,8 +4,6 @@ from app_modules.article_translator import ArticleTranslator
 
 if __name__ == '__main__':
 
-    articles = Article.get_by_status([ArticleStatus.RECOMMENDED])
-
-
+    articles = Article.get_by_status([ArticleStatus.RECOMMENDED], )
 
     ArticleTranslator.run_article_translation_for_default_langs(article, force, public)
