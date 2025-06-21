e83fc8c315bd3bddbe99e7ac02843c433dc48d41
ofa <fambon@github.users>
2025-06-21 14:32:59 +0200

    add global mem usage display in monitor-ram.sh

--- utils/mem/monitor-ram.sh
+++ utils/mem/monitor-ram.sh
@@ -3,6 +3,8 @@
 while true; do
     clear
     date '+%F %T'
+    free | awk '/Mem:/ {print "mem used: " int($3/$2*1000)/10 "%"}'
+    echo
     echo "%mem cmd"
     ps -ax -o %mem,cmd | sort -k1nr | head -30
     sleep 10
